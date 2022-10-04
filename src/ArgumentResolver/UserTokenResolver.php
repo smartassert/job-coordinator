f32c95b03d7d4dec4b9110d061f510a1f4b54c05
Jon Cram <webignition@gmail.com>
2022-10-04 10:27:08 +0100

    Create UserTokenResolver

---
+++ src/ArgumentResolver/UserTokenResolver.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ArgumentResolver;
+
+use SmartAssert\UsersSecurityBundle\Security\SymfonyRequestTokenExtractor;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+class UserTokenResolver implements ArgumentValueResolverInterface
+{
+    public function __construct(
+        private readonly SymfonyRequestTokenExtractor $tokenExtractor,
+    ) {
+    }
+
+    public function supports(Request $request, ArgumentMetadata $argument): bool
+    {
+        return 'string' === $argument->getType() && 'userToken' === $argument->getName();
+    }
+
+    /**
+     * @return \Traversable<?string>
+     */
+    public function resolve(Request $request, ArgumentMetadata $argument): \Traversable
+    {
+        yield $this->tokenExtractor->extract($request);
+    }
+}
