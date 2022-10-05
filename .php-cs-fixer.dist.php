1216ec774f80dc52b59c7a28f4bd949f0783a246
Jon Cram <webignition@gmail.com>
2022-10-05 15:35:06 +0100

    php-cs-fixer: no blank line before case statement

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -18,6 +18,30 @@ return (new PhpCsFixer\Config())
         ],
         'php_unit_internal_class' => false,
         'php_unit_test_class_requires_covers' => false,
+        'blank_line_before_statement' => [
+            'statements' => [
+                'break',
+                'continue',
+                'declare',
+                'default',
+                'phpdoc',
+                'do',
+                'exit',
+                'for',
+                'goto',
+                'include',
+                'include_once',
+                'require',
+                'require_once',
+                'return',
+                'switch',
+                'throw',
+                'try',
+                'while',
+                'yield',
+                'yield_from',
+            ],
+        ],
     ])
     ->setFinder($finder)
     ;
