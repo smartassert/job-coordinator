60eb75fe1876fdd3a1acd0f38e6a33c870a62879
Jon Cram <webignition@gmail.com>
2023-03-22 13:20:29 +0000

    Include machine in MachineStateChangeEvent (#157)

--- src/Event/MachineStateChangeEvent.php
+++ src/Event/MachineStateChangeEvent.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Event;
 
+use SmartAssert\WorkerManagerClient\Model\Machine;
 use Symfony\Contracts\EventDispatcher\Event;
 
 class MachineStateChangeEvent extends Event
 {
     public function __construct(
+        public readonly Machine $machine,
         public readonly ?string $previous,
-        public readonly string $current,
     ) {
     }
 }
