29ebe3e164d8c006dda03e8d8e549912d52f6859
Jon Cram <webignition@gmail.com>
2023-04-28 12:12:29 +0100

    Split CreateJobSuccessTest into many test methods (#245)

--- tests/Application/AbstractCreateJobSuccessTest.php
+++ tests/Application/AbstractCreateJobSuccessTest.php
@@ -6,24 +6,37 @@ namespace App\Tests\Application;
 
 use App\Entity\Job;
 use App\Repository\JobRepository;
+use Psr\Http\Message\ResponseInterface;
 use SmartAssert\SourcesClient\FileClient;
-use SmartAssert\SourcesClient\SerializedSuiteClient;
+use SmartAssert\SourcesClient\Model\Suite;
 use SmartAssert\SourcesClient\SourceClient;
 use SmartAssert\SourcesClient\SuiteClient;
 use SmartAssert\TestAuthenticationProviderBundle\ApiTokenProvider;
 use SmartAssert\TestAuthenticationProviderBundle\UserProvider;
-use Symfony\Component\Uid\Ulid;
+use SmartAssert\UsersClient\Model\User;
 
 abstract class AbstractCreateJobSuccessTest extends AbstractApplicationTest
 {
-    public function testCreateSuccess(): void
+    private static ResponseInterface $createResponse;
+    private static User $user;
+    private static Suite $suite;
+
+    /**
+     * @var array<mixed>
+     */
+    private static array $createResponseData;
+
+    public static function setUpBeforeClass(): void
     {
+        parent::setUpBeforeClass();
+
         $apiTokenProvider = self::getContainer()->get(ApiTokenProvider::class);
         \assert($apiTokenProvider instanceof ApiTokenProvider);
         $apiToken = $apiTokenProvider->get('user@example.com');
 
         $userProvider = self::getContainer()->get(UserProvider::class);
         \assert($userProvider instanceof UserProvider);
+        self::$user = $userProvider->get('user@example.com');
 
         $sourceClient = self::getContainer()->get(SourceClient::class);
         \assert($sourceClient instanceof SourceClient);
@@ -35,59 +48,79 @@ abstract class AbstractCreateJobSuccessTest extends AbstractApplicationTest
 
         $suiteClient = self::getContainer()->get(SuiteClient::class);
         \assert($suiteClient instanceof SuiteClient);
-        $suite = $suiteClient->create($apiToken, $source->getId(), md5((string) rand()), ['test1.yaml']);
+        self::$suite = $suiteClient->create($apiToken, $source->getId(), md5((string) rand()), ['test1.yaml']);
 
         $jobRepository = self::getContainer()->get(JobRepository::class);
         \assert($jobRepository instanceof JobRepository);
         self::assertCount(0, $jobRepository->findAll());
 
-        $response = self::$staticApplicationClient->makeCreateJobRequest($apiToken, $suite->getId());
+        self::$createResponse = self::$staticApplicationClient->makeCreateJobRequest($apiToken, self::$suite->getId());
 
-        self::assertSame(200, $response->getStatusCode());
-        self::assertSame('application/json', $response->getHeaderLine('content-type'));
+        self::assertSame(200, self::$createResponse->getStatusCode());
+        self::assertSame('application/json', self::$createResponse->getHeaderLine('content-type'));
 
-        $responseData = json_decode($response->getBody()->getContents(), true);
+        $responseData = json_decode(self::$createResponse->getBody()->getContents(), true);
         self::assertIsArray($responseData);
+        self::$createResponseData = $responseData;
+    }
 
-        self::assertArrayHasKey('job', $responseData);
-        $jobData = $responseData['job'];
-
-        self::assertArrayHasKey('id', $jobData);
-        self::assertTrue(Ulid::isValid($jobData['id']));
-
-        self::assertArrayHasKey('suite_id', $jobData);
-        self::assertSame($suite->getId(), $jobData['suite_id']);
-
-        self::assertArrayHasKey('serialized_suite_id', $jobData);
-        $serializedSuiteId = $jobData['serialized_suite_id'];
-
-        self::assertArrayHasKey('machine', $responseData);
-        $machineData = $responseData['machine'];
-
-        self::assertArrayHasKey('id', $machineData);
-        self::assertSame($jobData['id'], $machineData['id']);
-
-        self::assertArrayHasKey('state', $machineData);
-        self::assertSame('create/received', $machineData['state']);
-
-        self::assertArrayHasKey('ip_addresses', $machineData);
-        self::assertSame([], $machineData['ip_addresses']);
+    public function testJobIsCreated(): void
+    {
+        $jobRepository = self::getContainer()->get(JobRepository::class);
+        \assert($jobRepository instanceof JobRepository);
+        self::assertCount(1, $jobRepository->findAll());
 
         $jobs = $jobRepository->findAll();
-        self::assertCount(1, $jobs);
-
-        $job = $jobs[0];
+        $job = $jobs[0] ?? null;
         self::assertInstanceOf(Job::class, $job);
-        self::assertSame($job->userId, $userProvider->get('user@example.com')->id);
-        self::assertSame($job->suiteId, $suite->getId());
+    }
+
+    public function testJobUser(): void
+    {
+        $job = $this->getJob();
+        \assert($job instanceof Job);
+
+        self::assertSame($job->userId, self::$user->id);
+    }
+
+    public function testJobResultsTokenIsSet(): void
+    {
+        $job = $this->getJob();
+        \assert($job instanceof Job);
+
         self::assertNotNull($job->resultsToken);
+    }
 
-        $serializedSuiteClient = self::getContainer()->get(SerializedSuiteClient::class);
-        \assert($serializedSuiteClient instanceof SerializedSuiteClient);
+    public function testJobResponseData(): void
+    {
+        $job = $this->getJob();
+        \assert($job instanceof Job);
 
-        $serializedSuite = $serializedSuiteClient->get($apiToken, $serializedSuiteId);
+        self::assertSame(
+            [
+                'job' => [
+                    'id' => $job->id,
+                    'suite_id' => $job->suiteId,
+                    'serialized_suite_id' => $job->serializedSuiteId,
+                ],
+                'machine' => [
+                    'id' => $job->id,
+                    'state' => 'create/received',
+                    'state_category' => 'pre_active',
+                    'ip_addresses' => [],
+                ],
+            ],
+            self::$createResponseData,
+        );
+    }
 
-        self::assertSame($serializedSuiteId, $serializedSuite->getId());
-        self::assertSame($suite->getId(), $serializedSuite->getSuiteId());
+    private function getJob(): ?Job
+    {
+        $jobRepository = self::getContainer()->get(JobRepository::class);
+        \assert($jobRepository instanceof JobRepository);
+        $jobs = $jobRepository->findAll();
+        $job = $jobs[0] ?? null;
+
+        return $job instanceof Job ? $job : null;
     }
 }
