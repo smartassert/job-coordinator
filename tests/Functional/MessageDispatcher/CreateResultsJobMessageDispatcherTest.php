cca6ff6cb0bdd9ba9f08d21b5c15f109a7359835
Jon Cram <webignition@gmail.com>
2023-05-05 16:17:29 +0100

    Dispatch CreateResultsJobMessage on JobCreatedEvent (#310)

---
+++ tests/Functional/MessageDispatcher/CreateResultsJobMessageDispatcherTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\MessageDispatcher;
+
+use App\Entity\Job;
+use App\Event\JobCreatedEvent;
+use App\Message\CreateResultsJobMessage;
+use App\MessageDispatcher\CreateResultsJobMessageDispatcher;
+use App\Repository\JobRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Stamp\DelayStamp;
+use Symfony\Component\Messenger\Transport\InMemoryTransport;
+
+class CreateResultsJobMessageDispatcherTest extends WebTestCase
+{
+    private CreateResultsJobMessageDispatcher $dispatcher;
+    private InMemoryTransport $messengerTransport;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $dispatcher = self::getContainer()->get(CreateResultsJobMessageDispatcher::class);
+        \assert($dispatcher instanceof CreateResultsJobMessageDispatcher);
+        $this->dispatcher = $dispatcher;
+
+        $messengerTransport = self::getContainer()->get('messenger.transport.async');
+        \assert($messengerTransport instanceof InMemoryTransport);
+        $this->messengerTransport = $messengerTransport;
+    }
+
+    public function testIsEventSubscriber(): void
+    {
+        self::assertInstanceOf(EventSubscriberInterface::class, $this->dispatcher);
+        self::assertArrayHasKey(JobCreatedEvent::class, $this->dispatcher::getSubscribedEvents());
+    }
+
+    public function testDispatchForJobCreatedEventSuccess(): void
+    {
+        $jobId = md5((string) rand());
+        $job = new Job($jobId, 'user id', 'suite id', 600);
+        $jobRepository = self::getContainer()->get(JobRepository::class);
+        \assert($jobRepository instanceof JobRepository);
+        $jobRepository->add($job);
+
+        $authenticationToken = md5((string) rand());
+
+        $event = new JobCreatedEvent($authenticationToken, $jobId);
+
+        $this->dispatcher->dispatchForJobCreatedEvent($event);
+
+        $envelopes = $this->messengerTransport->get();
+        self::assertIsArray($envelopes);
+        self::assertCount(1, $envelopes);
+
+        $expectedMessage = new CreateResultsJobMessage($authenticationToken, $jobId);
+
+        $dispatchedEnvelope = $envelopes[0];
+        self::assertInstanceOf(Envelope::class, $dispatchedEnvelope);
+        self::assertEquals($expectedMessage, $dispatchedEnvelope->getMessage());
+
+        self::assertSame([], $dispatchedEnvelope->all(DelayStamp::class));
+    }
+}
