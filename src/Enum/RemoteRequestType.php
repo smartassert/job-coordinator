c842ca51ad72ea708df24da999eb032e98fde296
Jon Cram <webignition@gmail.com>
2023-05-19 11:52:25 +0100

    Create RemoteRequest entity (#371)

---
+++ src/Enum/RemoteRequestType.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum RemoteRequestType: string
+{
+    case MACHINE_CREATE = 'machine/create';
+    case MACHINE_GET = 'machine/get';
+    case MACHINE_START_JOB = 'machine/start-job';
+    case RESULTS_CREATE = 'results/create';
+    case SERIALIZED_SUITE_CREATE = 'serialized-suite/create';
+    case SERIALIZED_SUITE_READ = 'serialized-suite/read';
+    case SERIALIZED_SUITE_GET = 'serialized-suite/get';
+}
