94f6f76bd9431c556596c2f84ec4b21cb3413e55
Jon Cram <webignition@gmail.com>
2023-05-19 11:52:25 +0100

    Create RemoteRequest entity (#371)

---
+++ src/Repository/RemoteRequestRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\RemoteRequest;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<RemoteRequest>
+ *
+ * @method null|RemoteRequest find($id, $lockMode = null, $lockVersion = null)
+ * @method null|RemoteRequest findOneBy(array $criteria, array $orderBy = null)
+ * @method RemoteRequest[]    findAll()
+ * @method RemoteRequest[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RemoteRequestRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, RemoteRequest::class);
+    }
+
+    public function save(RemoteRequest $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(RemoteRequest $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
