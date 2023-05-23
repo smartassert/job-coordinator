cbce5d3a0aadf968beccbb58bf05728cde0d2ad2
Jon Cram <webignition@gmail.com>
2023-05-23 13:59:11 +0100

    RemoteRequest::id is a concatenation of job id and type (#398)

--- src/Services/RemoteRequestFactory.php
+++ src/Services/RemoteRequestFactory.php
@@ -6,31 +6,24 @@ namespace App\Services;
 
 use App\Entity\RemoteRequest;
 use App\Enum\RemoteRequestType;
-use App\Exception\EmptyUlidException;
 use App\Repository\RemoteRequestRepository;
 
 class RemoteRequestFactory
 {
     public function __construct(
         private readonly RemoteRequestRepository $repository,
-        private readonly UlidFactory $ulidFactory,
     ) {
     }
 
     /**
      * @param non-empty-string $jobId
-     *
-     * @throws EmptyUlidException
      */
     public function create(string $jobId, RemoteRequestType $type): RemoteRequest
     {
-        $remoteRequest = $this->repository->findOneBy([
-            'jobId' => $jobId,
-            'type' => $type,
-        ]);
+        $remoteRequest = $this->repository->find(RemoteRequest::generateId($jobId, $type));
 
         if (null === $remoteRequest) {
-            $remoteRequest = new RemoteRequest($this->ulidFactory->create(), $jobId, $type);
+            $remoteRequest = new RemoteRequest($jobId, $type);
             $this->repository->save($remoteRequest);
         }
 
