3e2f8deaa7e8847c1e33a773e1fe43eb97866da3
Jon Cram <webignition@gmail.com>
2023-05-24 11:21:33 +0100

    Perist GetMachine request state (#402)

--- src/MessageHandler/GetMachineMessageHandler.php
+++ src/MessageHandler/GetMachineMessageHandler.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace App\MessageHandler;
 
+use App\Enum\RemoteRequestType;
 use App\Event\MachineRetrievedEvent;
+use App\Event\RemoteRequestCompletedEvent;
+use App\Event\RemoteRequestFailedEvent;
+use App\Event\RemoteRequestStartedEvent;
 use App\Exception\MachineRetrievalException;
 use App\Message\GetMachineMessage;
 use App\Repository\JobRepository;
+use App\Services\RemoteRequestFactory;
 use SmartAssert\WorkerManagerClient\Client as WorkerManagerClient;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -19,6 +24,7 @@ final class GetMachineMessageHandler
         private readonly JobRepository $jobRepository,
         private readonly WorkerManagerClient $workerManagerClient,
         private readonly EventDispatcherInterface $eventDispatcher,
+        private readonly RemoteRequestFactory $remoteRequestFactory,
     ) {
     }
 
@@ -34,15 +40,22 @@ final class GetMachineMessageHandler
 
         $previousMachine = $message->machine;
 
+        $remoteRequest = $this->remoteRequestFactory->create($message->machine->id, RemoteRequestType::MACHINE_GET);
+
+        $this->eventDispatcher->dispatch(new RemoteRequestStartedEvent($remoteRequest));
+
         try {
             $machine = $this->workerManagerClient->getMachine($message->authenticationToken, $message->getJobId());
 
+            $this->eventDispatcher->dispatch(new RemoteRequestCompletedEvent($remoteRequest));
             $this->eventDispatcher->dispatch(new MachineRetrievedEvent(
                 $message->authenticationToken,
                 $previousMachine,
                 $machine
             ));
         } catch (\Throwable $e) {
+            $this->eventDispatcher->dispatch(new RemoteRequestFailedEvent($remoteRequest));
+
             throw new MachineRetrievalException($job, $previousMachine, $e);
         }
     }
