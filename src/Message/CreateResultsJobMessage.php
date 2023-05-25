5ad303f1a2b145d3793fc73f90918769a2f50cd2
Jon Cram <webignition@gmail.com>
2023-05-25 16:23:05 +0100

    Add GetMachineMessage::jobId (#426)

--- src/Message/CreateResultsJobMessage.php
+++ src/Message/CreateResultsJobMessage.php
@@ -6,23 +6,8 @@ namespace App\Message;
 
 use App\Enum\RemoteRequestType;
 
-class CreateResultsJobMessage implements JobRemoteRequestMessageInterface
+class CreateResultsJobMessage extends AbstractRemoteRequestMessage
 {
-    /**
-     * @param non-empty-string $authenticationToken
-     * @param non-empty-string $jobId
-     */
-    public function __construct(
-        public readonly string $authenticationToken,
-        private readonly string $jobId,
-    ) {
-    }
-
-    public function getJobId(): string
-    {
-        return $this->jobId;
-    }
-
     public function getRemoteRequestType(): RemoteRequestType
     {
         return RemoteRequestType::RESULTS_CREATE;
