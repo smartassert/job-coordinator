e4cfddc4fb4db051797493a85ecc8b0e0c451f6b
Jon Cram <webignition@gmail.com>
2023-05-25 16:23:05 +0100

    Add GetMachineMessage::jobId (#426)

--- tests/Functional/MessageDispatcher/GetMachineMessageDispatcherTest.php
+++ tests/Functional/MessageDispatcher/GetMachineMessageDispatcherTest.php
@@ -60,7 +60,7 @@ class GetMachineMessageDispatcherTest extends WebTestCase
         self::assertIsArray($envelopes);
         self::assertCount(1, $envelopes);
 
-        $expectedMessage = new GetMachineMessage($authenticationToken, $machine);
+        $expectedMessage = new GetMachineMessage($authenticationToken, $machine->id, $machine);
 
         $dispatchedEnvelope = $envelopes[0];
         self::assertInstanceOf(Envelope::class, $dispatchedEnvelope);
