8a05485d2144e4816b2db6fbd5410d9e321f84ef
Jon Cram <webignition@gmail.com>
2023-05-26 15:21:24 +0100

    Create RemoteRequestIndexGenerator (#439)

---
+++ src/Services/RemoteRequestIndexGenerator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Enum\RemoteRequestType;
+use App\Repository\RemoteRequestRepository;
+
+class RemoteRequestIndexGenerator
+{
+    public function __construct(
+        private readonly RemoteRequestRepository $remoteRequestRepository,
+    ) {
+    }
+
+    /**
+     * @return int<0, max>
+     */
+    public function generate(string $jobId, RemoteRequestType $type): int
+    {
+        $largestIndex = $this->remoteRequestRepository->getLargestIndex($jobId, $type);
+
+        return null === $largestIndex ? 0 : $largestIndex + 1;
+    }
+}
