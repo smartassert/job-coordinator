ce4271aa2488c766cd0b29070d310ccb95f90c05
Jon Cram <webignition@gmail.com>
2023-06-01 17:49:25 +0100

    Create RemoteRequestFailureFactory (#449)

---
+++ src/Services/RemoteRequestFailureFactory/HttpResponseExceptionHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\RemoteRequestFailureFactory;
+
+use App\Enum\RemoteRequestFailureType;
+use App\Model\RemoteRequestFailure;
+use SmartAssert\ServiceClient\Exception\HttpResponseExceptionInterface;
+
+class HttpResponseExceptionHandler implements ExceptionHandlerInterface
+{
+    public function handle(\Throwable $throwable): ?RemoteRequestFailure
+    {
+        if (!$throwable instanceof HttpResponseExceptionInterface) {
+            return null;
+        }
+
+        return new RemoteRequestFailure(
+            RemoteRequestFailureType::HTTP,
+            $throwable->getStatusCode(),
+            $throwable->getReasonPhrase()
+        );
+    }
+}
