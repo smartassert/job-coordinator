2e272810105727c2e9cb9ed1f6c34e0f3e9ea203
Jon Cram <webignition@gmail.com>
2023-06-02 16:33:20 +0100

    Create RemoteRequestFailureRecorder (#479)

--- src/MessageFailureHandler/MachineCreationExceptionHandler.php
+++ src/MessageFailureHandler/MachineCreationExceptionHandler.php
@@ -4,20 +4,17 @@ declare(strict_types=1);
 
 namespace App\MessageFailureHandler;
 
-use App\Entity\RemoteRequest;
 use App\Enum\RequestState;
 use App\Exception\MachineCreationException;
 use App\Repository\JobRepository;
-use App\Repository\RemoteRequestRepository;
-use App\Services\RemoteRequestFailureFactory\RemoteRequestFailureFactory;
+use App\Services\RemoteRequestFailureRecorder;
 use SmartAssert\WorkerMessageFailedEventBundle\ExceptionHandlerInterface;
 
 class MachineCreationExceptionHandler implements ExceptionHandlerInterface
 {
     public function __construct(
         private readonly JobRepository $jobRepository,
-        private readonly RemoteRequestRepository $remoteRequestRepository,
-        private readonly RemoteRequestFailureFactory $remoteRequestFailureFactory,
+        private readonly RemoteRequestFailureRecorder $remoteRequestFailureRecorder,
     ) {
     }
 
@@ -27,18 +24,7 @@ class MachineCreationExceptionHandler implements ExceptionHandlerInterface
             return;
         }
 
-        $remoteRequest = $this->remoteRequestRepository->findOneBy([
-            'jobId' => $throwable->getJob()->id,
-        ]);
-
-        if ($remoteRequest instanceof RemoteRequest) {
-            $remoteRequestFailure = $this->remoteRequestFailureFactory->create($throwable->getPreviousException());
-
-            if (null !== $remoteRequestFailure) {
-                $remoteRequest->setFailure($remoteRequestFailure);
-                $this->remoteRequestRepository->save($remoteRequest);
-            }
-        }
+        $this->remoteRequestFailureRecorder->record($throwable);
 
         $throwable->getJob()->setMachineRequestState(RequestState::FAILED);
         $this->jobRepository->add($throwable->getJob());
