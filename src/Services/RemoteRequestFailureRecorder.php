6b5eefcde5cf24680781ab8982d5309c55804c2e
Jon Cram <webignition@gmail.com>
2023-06-02 16:33:20 +0100

    Create RemoteRequestFailureRecorder (#479)

---
+++ src/Services/RemoteRequestFailureRecorder.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Entity\RemoteRequest;
+use App\Exception\RemoteRequestExceptionInterface;
+use App\Repository\RemoteRequestRepository;
+use App\Services\RemoteRequestFailureFactory\RemoteRequestFailureFactory;
+
+class RemoteRequestFailureRecorder
+{
+    public function __construct(
+        private readonly RemoteRequestRepository $remoteRequestRepository,
+        private readonly RemoteRequestFailureFactory $remoteRequestFailureFactory,
+    ) {
+    }
+
+    public function record(RemoteRequestExceptionInterface $throwable): void
+    {
+        $remoteRequest = $this->remoteRequestRepository->findOneBy([
+            'jobId' => $throwable->getJob()->id,
+        ]);
+
+        if ($remoteRequest instanceof RemoteRequest) {
+            $remoteRequestFailure = $this->remoteRequestFailureFactory->create($throwable->getPreviousException());
+
+            if (null !== $remoteRequestFailure) {
+                $remoteRequest->setFailure($remoteRequestFailure);
+                $this->remoteRequestRepository->save($remoteRequest);
+            }
+        }
+    }
+}
