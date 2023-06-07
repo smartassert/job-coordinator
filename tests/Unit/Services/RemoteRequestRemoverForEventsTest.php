d3f81983efd195f256bde4ce96b03f831dc7b796
Jon Cram <webignition@gmail.com>
2023-06-07 17:05:24 +0100

    Remove serialized-suite/create request states on successful serialized suite creation (#528)

--- tests/Unit/Services/RemoteRequestRemoverForEventsTest.php
+++ tests/Unit/Services/RemoteRequestRemoverForEventsTest.php
@@ -6,9 +6,13 @@ namespace App\Tests\Unit\Services;
 
 use App\Enum\RemoteRequestType;
 use App\Event\MachineIsActiveEvent;
+use App\Event\ResultsJobCreatedEvent;
+use App\Event\SerializedSuiteCreatedEvent;
 use App\Services\RemoteRequestRemover;
 use App\Services\RemoteRequestRemoverForEvents;
 use PHPUnit\Framework\TestCase;
+use SmartAssert\ResultsClient\Model\Job as ResultsJob;
+use SmartAssert\SourcesClient\Model\SerializedSuite;
 
 class RemoteRequestRemoverForEventsTest extends TestCase
 {
@@ -27,6 +31,46 @@ class RemoteRequestRemoverForEventsTest extends TestCase
 
         $remoteRequestRemoverForEvents->removeMachineCreateRemoteRequestsForMachineIsActiveEvent(
             new MachineIsActiveEvent('authentication token', $jobId, '127.0.0.1')
+        );
+
+        self::assertTrue(true);
+    }
+
+    public function testRemoveResultsCreateRemoteRequestsForResultsJobCreatedEvent(): void
+    {
+        $jobId = md5((string) rand());
+
+        $remoteRequestRemover = \Mockery::mock(RemoteRequestRemover::class);
+        $remoteRequestRemover
+            ->shouldReceive('removeForJobAndType')
+            ->with($jobId, RemoteRequestType::RESULTS_CREATE)
+            ->andReturn([])
+        ;
+
+        $remoteRequestRemoverForEvents = new RemoteRequestRemoverForEvents($remoteRequestRemover);
+
+        $remoteRequestRemoverForEvents->removeResultsCreateRemoteRequestsForResultsJobCreatedEvent(
+            new ResultsJobCreatedEvent('authentication token', $jobId, \Mockery::mock(ResultsJob::class))
+        );
+
+        self::assertTrue(true);
+    }
+
+    public function testRemoveSerializedSuiteCreateRequestsForSerializedSuiteCreatedEvent(): void
+    {
+        $jobId = md5((string) rand());
+
+        $remoteRequestRemover = \Mockery::mock(RemoteRequestRemover::class);
+        $remoteRequestRemover
+            ->shouldReceive('removeForJobAndType')
+            ->with($jobId, RemoteRequestType::SERIALIZED_SUITE_CREATE)
+            ->andReturn([])
+        ;
+
+        $remoteRequestRemoverForEvents = new RemoteRequestRemoverForEvents($remoteRequestRemover);
+
+        $remoteRequestRemoverForEvents->removeSerializedSuiteCreateRequestsForSerializedSuiteCreatedEvent(
+            new SerializedSuiteCreatedEvent('authentication token', $jobId, \Mockery::mock(SerializedSuite::class))
         );
 
         self::assertTrue(true);
