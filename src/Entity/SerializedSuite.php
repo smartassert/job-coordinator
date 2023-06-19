5f1037810a8ca82ba5d873644c4306dee9eda175
Jon Cram <webignition@gmail.com>
2023-06-19 10:50:35 +0100

    Create SerializedSuite entity

---
+++ src/Entity/SerializedSuite.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\SerializedSuiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SerializedSuiteRepository::class)]
+class SerializedSuite
+{
+    /**
+     * @var non-empty-string
+     */
+    #[ORM\Id]
+    #[ORM\Column(length: 32, unique: true, nullable: false)]
+    private string $jobId;
+
+    /**
+     * @var non-empty-string
+     */
+    #[ORM\Column(length: 32, unique: true, nullable: false)]
+    private string $serializedSuiteId;
+
+    /**
+     * @var non-empty-string
+     */
+    #[ORM\Column(length: 128, nullable: false)]
+    private string $state;
+
+    /**
+     * @param non-empty-string $jobId
+     * @param non-empty-string $serializedSuiteId
+     * @param non-empty-string $state
+     */
+    public function __construct(
+        string $jobId,
+        string $serializedSuiteId,
+        string $state,
+    ) {
+        $this->jobId = $jobId;
+        $this->serializedSuiteId = $serializedSuiteId;
+        $this->state = $state;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param non-empty-string $state
+     */
+    public function setState(string $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
