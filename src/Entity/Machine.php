19250645f72e829e6ad63cdd1dd3b3f83d86f1f9
Jon Cram <webignition@gmail.com>
2023-06-21 10:00:27 +0100

    Create Machine entity

---
+++ src/Entity/Machine.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\MachineRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MachineRepository::class)]
+class Machine
+{
+    /**
+     * @var non-empty-string
+     */
+    #[ORM\Id]
+    #[ORM\Column(length: 32, unique: true, nullable: false)]
+    private readonly string $jobId;
+
+    /**
+     * @var non-empty-string
+     */
+    #[ORM\Column(length: 128, nullable: false)]
+    private string $state;
+
+    /**
+     * @var non-empty-string
+     */
+    #[ORM\Column(length: 128, nullable: false)]
+    private string $stateCategory;
+
+    /**
+     * @var ?non-empty-string
+     */
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $ip = null;
+
+    /**
+     * @param non-empty-string $jobId
+     * @param non-empty-string $state
+     * @param non-empty-string $stateCategory
+     */
+    public function __construct(string $jobId, string $state, string $stateCategory)
+    {
+        $this->jobId = $jobId;
+        $this->state = $state;
+        $this->stateCategory = $stateCategory;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getId(): string
+    {
+        return $this->jobId;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param non-empty-string $state
+     */
+    public function setState(string $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getStateCategory(): string
+    {
+        return $this->stateCategory;
+    }
+
+    /**
+     * @param non-empty-string $stateCategory
+     */
+    public function setStateCategory(string $stateCategory): static
+    {
+        $this->state = $stateCategory;
+
+        return $this;
+    }
+
+    /**
+     * @return ?non-empty-string
+     */
+    public function getIp(): ?string
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @param non-empty-string $ip
+     */
+    public function setIp(string $ip): static
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+}
---
+++ src/Repository/MachineRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Machine;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Machine>
+ *
+ * @method null|Machine find($id, $lockMode = null, $lockVersion = null)
+ * @method null|Machine findOneBy(array $criteria, array $orderBy = null)
+ * @method Machine[]    findAll()
+ * @method Machine[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MachineRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Machine::class);
+    }
+
+    public function save(Machine $entity): void
+    {
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+    }
+}
