7b8887263b3a38813c420c69050d933ee4fe9aee
Jon Cram <webignition@gmail.com>
2023-06-27 17:39:28 +0100

    Remove usage when updating state of existing request

--- src/Services/RemoteRequestStateTracker.php
+++ src/Services/RemoteRequestStateTracker.php
@@ -10,9 +10,9 @@ use App\Event\JobRemoteRequestMessageCreatedEvent;
 use App\Message\JobRemoteRequestMessageInterface;
 use App\Repository\RemoteRequestRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Messenger\Event\WorkerMessageFailedEvent as FailedEvent;
-use Symfony\Component\Messenger\Event\WorkerMessageHandledEvent as HandledEvent;
-use Symfony\Component\Messenger\Event\WorkerMessageReceivedEvent as ReceivedEvent;
+use Symfony\Component\Messenger\Event\WorkerMessageFailedEvent;
+use Symfony\Component\Messenger\Event\WorkerMessageHandledEvent;
+use Symfony\Component\Messenger\Event\WorkerMessageReceivedEvent;
 
 class RemoteRequestStateTracker implements EventSubscriberInterface
 {
@@ -28,14 +28,14 @@ class RemoteRequestStateTracker implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            FailedEvent::class => [
-                ['setRemoteRequestStateForMessengerEvent', 10000],
+            WorkerMessageFailedEvent::class => [
+                ['setRemoteRequestStateForMessageFailedEvent', 10000],
             ],
-            HandledEvent::class => [
-                ['setRemoteRequestStateForMessengerEvent', 10000],
+            WorkerMessageHandledEvent::class => [
+                ['setRemoteRequestStateForMessageHandledEvent', 10000],
             ],
-            ReceivedEvent::class => [
-                ['setRemoteRequestStateForMessengerEvent', 10000],
+            WorkerMessageReceivedEvent::class => [
+                ['setRemoteRequestStateForMessageReceivedEvent', 10000],
             ],
             JobRemoteRequestMessageCreatedEvent::class => [
                 ['setRemoteRequestStateForJobRemoteRequestMessageCreatedEvent', 10000],
@@ -43,14 +43,34 @@ class RemoteRequestStateTracker implements EventSubscriberInterface
         ];
     }
 
-    public function setRemoteRequestStateForMessengerEvent(FailedEvent|HandledEvent|ReceivedEvent $event): void
+    public function setRemoteRequestStateForMessageFailedEvent(WorkerMessageFailedEvent $event): void
     {
         $message = $event->getEnvelope()->getMessage();
         if (!$message instanceof JobRemoteRequestMessageInterface) {
             return;
         }
 
-        $this->setRemoteRequestForMessage($message, $this->getRequestStateFromEvent($event));
+        $this->setRemoteRequestForMessage($message, $event->willRetry() ? RequestState::HALTED : RequestState::FAILED);
+    }
+
+    public function setRemoteRequestStateForMessageHandledEvent(WorkerMessageHandledEvent $event): void
+    {
+        $message = $event->getEnvelope()->getMessage();
+        if (!$message instanceof JobRemoteRequestMessageInterface) {
+            return;
+        }
+
+        $this->setRemoteRequestForMessage($message, RequestState::SUCCEEDED);
+    }
+
+    public function setRemoteRequestStateForMessageReceivedEvent(WorkerMessageReceivedEvent $event): void
+    {
+        $message = $event->getEnvelope()->getMessage();
+        if (!$message instanceof JobRemoteRequestMessageInterface) {
+            return;
+        }
+
+        $this->setRemoteRequestForMessage($message, RequestState::REQUESTING);
     }
 
     public function setRemoteRequestStateForJobRemoteRequestMessageCreatedEvent(
@@ -71,23 +91,6 @@ class RemoteRequestStateTracker implements EventSubscriberInterface
         $remoteRequest = $this->createRemoteRequest($message);
         $remoteRequest->setState($requestState);
         $this->remoteRequestRepository->save($remoteRequest);
-    }
-
-    private function getRequestStateFromEvent(object $event): RequestState
-    {
-        if ($event instanceof FailedEvent) {
-            return $event->willRetry() ? RequestState::HALTED : RequestState::FAILED;
-        }
-
-        if ($event instanceof HandledEvent) {
-            return RequestState::SUCCEEDED;
-        }
-
-        if ($event instanceof ReceivedEvent) {
-            return RequestState::REQUESTING;
-        }
-
-        return RequestState::UNKNOWN;
     }
 
     private function createRemoteRequest(JobRemoteRequestMessageInterface $message): RemoteRequest
