59e965dddc75062ea3eee42385c3a51d9ccf9a96
Jon Cram <webignition@gmail.com>
2023-06-28 17:53:30 +0100

    Add request to existing results_job, serialized_suite and machine objects with a state of complete (#618)

--- src/Services/JobSerializer.php
+++ src/Services/JobSerializer.php
@@ -8,9 +8,11 @@ use App\Entity\Job;
 use App\Entity\Machine as MachineEntity;
 use App\Entity\ResultsJob as ResultsJobEntity;
 use App\Entity\SerializedSuite as SerializedSuiteEntity;
+use App\Enum\RequestState;
 use App\Model\Machine as MachineModel;
 use App\Model\RemoteRequestCollection;
 use App\Model\ResultsJob as ResultsJobModel;
+use App\Model\SerializableRemoteRequest;
 use App\Model\SerializedSuite as SerializedSuiteModel;
 use App\Repository\MachineRepository;
 use App\Repository\RemoteRequestRepository;
@@ -50,17 +52,32 @@ class JobSerializer
 
         $resultsJob = $this->resultsJobRepository->find($job->id);
         if ($resultsJob instanceof ResultsJobEntity) {
-            $data['results_job'] = (new ResultsJobModel($resultsJob))->toArray();
+            $resultsJobModel = new ResultsJobModel(
+                $resultsJob,
+                new SerializableRemoteRequest(RequestState::SUCCEEDED),
+            );
+
+            $data['results_job'] = $resultsJobModel->toArray();
         }
 
         $serializedSuite = $this->serializedSuiteRepository->find($job->id);
         if ($serializedSuite instanceof SerializedSuiteEntity) {
-            $data['serialized_suite'] = (new SerializedSuiteModel($serializedSuite))->toArray();
+            $serializedSuiteModel = new SerializedSuiteModel(
+                $serializedSuite,
+                new SerializableRemoteRequest(RequestState::SUCCEEDED),
+            );
+
+            $data['serialized_suite'] = $serializedSuiteModel->toArray();
         }
 
         $machine = $this->machineRepository->find($job->id);
         if ($machine instanceof MachineEntity) {
-            $data['machine'] = (new MachineModel($machine))->toArray();
+            $machineModel = new MachineModel(
+                $machine,
+                new SerializableRemoteRequest(RequestState::SUCCEEDED),
+            );
+
+            $data['machine'] = $machineModel->toArray();
         }
 
         $remoteRequests = $this->remoteRequestRepository->findBy(['jobId' => $job->id], ['id' => 'ASC']);
