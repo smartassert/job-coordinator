b070df0e1684ffdea372bf4de2371538672d3ac6
Jon Cram <webignition@gmail.com>
2023-06-28 16:27:38 +0100

    Create model for serialized serialized suite (#614)

--- src/Services/JobSerializer.php
+++ src/Services/JobSerializer.php
@@ -7,9 +7,10 @@ namespace App\Services;
 use App\Entity\Job;
 use App\Entity\Machine;
 use App\Entity\ResultsJob as ResultsJobEntity;
-use App\Entity\SerializedSuite;
+use App\Entity\SerializedSuite as SerializedSuiteEntity;
 use App\Model\RemoteRequestCollection;
 use App\Model\ResultsJob as ResultsJobModel;
+use App\Model\SerializedSuite as SerializedSuiteModel;
 use App\Repository\MachineRepository;
 use App\Repository\RemoteRequestRepository;
 use App\Repository\ResultsJobRepository;
@@ -17,7 +18,7 @@ use App\Repository\SerializedSuiteRepository;
 
 /**
  * @phpstan-import-type SerializedResultsJob from ResultsJobModel
- * @phpstan-import-type SerializedSerializedSuite from SerializedSuite
+ * @phpstan-import-type SerializedSerializedSuite from SerializedSuiteModel
  * @phpstan-import-type SerializedMachine from Machine
  * @phpstan-import-type SerializedRemoteRequestCollection from RemoteRequestCollection
  */
@@ -52,8 +53,8 @@ class JobSerializer
         }
 
         $serializedSuite = $this->serializedSuiteRepository->find($job->id);
-        if ($serializedSuite instanceof SerializedSuite) {
-            $data['serialized_suite'] = $serializedSuite->toArray();
+        if ($serializedSuite instanceof SerializedSuiteEntity) {
+            $data['serialized_suite'] = (new SerializedSuiteModel($serializedSuite))->toArray();
         }
 
         $machine = $this->machineRepository->find($job->id);
