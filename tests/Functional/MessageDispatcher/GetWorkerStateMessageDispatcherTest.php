89cdd62bc372a40ca70895dcdc8f99dccee543eb
Jon Cram <webignition@gmail.com>
2023-07-05 16:50:15 +0100

    Dispatch GetWorkerStateMessage on successful worker job creation (#646)

---
+++ tests/Functional/MessageDispatcher/GetWorkerStateMessageDispatcherTest.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\MessageDispatcher;
+
+use App\Entity\Job;
+use App\Event\WorkerJobStartRequestedEvent;
+use App\Message\GetWorkerStateMessage;
+use App\MessageDispatcher\GetWorkerStateMessageDispatcher;
+use App\Repository\JobRepository;
+use SmartAssert\ResultsClient\Model\Job as ResultsJob;
+use SmartAssert\ResultsClient\Model\JobState as ResultsJobState;
+use SmartAssert\WorkerClient\Model\Job as WorkerJob;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Transport\InMemory\InMemoryTransport;
+
+class GetWorkerStateMessageDispatcherTest extends WebTestCase
+{
+    private GetWorkerStateMessageDispatcher $dispatcher;
+    private InMemoryTransport $messengerTransport;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $dispatcher = self::getContainer()->get(GetWorkerStateMessageDispatcher::class);
+        \assert($dispatcher instanceof GetWorkerStateMessageDispatcher);
+        $this->dispatcher = $dispatcher;
+
+        $messengerTransport = self::getContainer()->get('messenger.transport.async');
+        \assert($messengerTransport instanceof InMemoryTransport);
+        $this->messengerTransport = $messengerTransport;
+    }
+
+    public function testIsEventSubscriber(): void
+    {
+        self::assertInstanceOf(EventSubscriberInterface::class, $this->dispatcher);
+    }
+
+    /**
+     * @dataProvider eventSubscriptionsDataProvider
+     */
+    public function testEventSubscriptions(string $expectedListenedForEvent, string $expectedMethod): void
+    {
+        $subscribedEvents = $this->dispatcher::getSubscribedEvents();
+        self::assertArrayHasKey($expectedListenedForEvent, $subscribedEvents);
+
+        $eventSubscriptions = $subscribedEvents[$expectedListenedForEvent];
+        self::assertIsArray($eventSubscriptions);
+        self::assertIsArray($eventSubscriptions[0]);
+
+        $eventSubscription = $eventSubscriptions[0];
+        self::assertSame($expectedMethod, $eventSubscription[0]);
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function eventSubscriptionsDataProvider(): array
+    {
+        return [
+            WorkerJobStartRequestedEvent::class => [
+                'expectedListenedForEvent' => WorkerJobStartRequestedEvent::class,
+                'expectedMethod' => 'dispatchForWorkerJobStartRequestedEvent',
+            ],
+        ];
+    }
+
+    public function testDispatchForWorkerJobStartRequestedEventSuccess(): void
+    {
+        $jobId = md5((string) rand());
+        $job = new Job($jobId, 'user id', 'suite id', 600);
+        $jobRepository = self::getContainer()->get(JobRepository::class);
+        \assert($jobRepository instanceof JobRepository);
+        $jobRepository->add($job);
+
+        $authenticationToken = md5((string) rand());
+        $resultsToken = md5((string) rand());
+        $resultsJob = new ResultsJob($jobId, $resultsToken, new ResultsJobState('awaiting-events', null));
+        $workerJob = \Mockery::mock(WorkerJob::class);
+
+        $event = new WorkerJobStartRequestedEvent($authenticationToken, $jobId, $workerJob);
+
+        $this->dispatcher->dispatchForWorkerJobStartRequestedEvent($event);
+
+        $this->assertDispatchedMessage(new GetWorkerStateMessage($authenticationToken, $jobId));
+    }
+
+    private function assertDispatchedMessage(GetWorkerStateMessage $expected): void
+    {
+        $envelopes = $this->messengerTransport->getSent();
+        self::assertIsArray($envelopes);
+        self::assertCount(1, $envelopes);
+
+        $dispatchedEnvelope = $envelopes[0];
+        self::assertInstanceOf(Envelope::class, $dispatchedEnvelope);
+        self::assertEquals($expected, $dispatchedEnvelope->getMessage());
+    }
+}
