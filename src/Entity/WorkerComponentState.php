23aaad91aa8d7929db195e2e7b7000696c91fc02
Jon Cram <webignition@gmail.com>
2023-07-10 11:09:32 +0100

    Create WorkerComponentState entity

---
+++ src/Entity/WorkerComponentState.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Enum\WorkerComponentName;
+use App\Repository\WorkerComponentStateRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WorkerComponentStateRepository::class)]
+class WorkerComponentState
+{
+    /**
+     * @var non-empty-string
+     */
+    #[ORM\Id]
+    #[ORM\Column(type: 'string', length: 32, unique: true)]
+    private string $id;
+
+    /**
+     * @var non-empty-string
+     */
+    #[ORM\Column(length: 64)]
+    private string $state;
+
+    #[ORM\Column]
+    private bool $isEndState;
+
+    /**
+     * @param non-empty-string $jobId
+     * @param non-empty-string $state
+     */
+    public function __construct(
+        string $jobId,
+        WorkerComponentName $componentName,
+        string $state,
+        bool $isEndState,
+    ) {
+        $this->id = static::generateId($jobId, $componentName);
+        $this->state = $state;
+        $this->isEndState = $isEndState;
+    }
+
+    /**
+     * @param non-empty-string $jobId
+     *
+     * @return non-empty-string
+     */
+    public static function generateId(string $jobId, WorkerComponentName $componentName): string
+    {
+        return md5($jobId . $componentName->value);
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param non-empty-string $state
+     */
+    public function setState(string $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getIsEndState(): bool
+    {
+        return $this->isEndState;
+    }
+
+    public function setIsEndState(bool $isEndState): static
+    {
+        $this->isEndState = $isEndState;
+
+        return $this;
+    }
+}
---
+++ src/Repository/WorkerComponentStateRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\WorkerComponentState;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<WorkerComponentState>
+ *
+ * @method null|WorkerComponentState find($id, $lockMode = null, $lockVersion = null)
+ * @method null|WorkerComponentState findOneBy(array $criteria, array $orderBy = null)
+ * @method WorkerComponentState[]    findAll()
+ * @method WorkerComponentState[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class WorkerComponentStateRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, WorkerComponentState::class);
+    }
+
+    public function save(WorkerComponentState $entity): void
+    {
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+    }
+}
