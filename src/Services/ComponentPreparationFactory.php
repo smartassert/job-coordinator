cb963e2b00326466fabd6942ab29249caad7c85a
Jon Cram <webignition@gmail.com>
2023-07-11 17:53:49 +0100

    Model component preparation state, include failure (#705)

--- src/Services/ComponentPreparationFactory.php
+++ src/Services/ComponentPreparationFactory.php
@@ -8,12 +8,13 @@ use App\Entity\Job;
 use App\Enum\PreparationState;
 use App\Enum\RemoteRequestType;
 use App\Enum\RequestState;
+use App\Model\ComponentPreparation;
 use App\Repository\MachineRepository;
 use App\Repository\RemoteRequestRepository;
 use App\Repository\ResultsJobRepository;
 use App\Repository\SerializedSuiteRepository;
 
-class PreparationStateDeriver
+class ComponentPreparationFactory
 {
     public function __construct(
         private readonly ResultsJobRepository $resultsJobRepository,
@@ -23,57 +24,57 @@ class PreparationStateDeriver
     ) {
     }
 
-    public function getForResultsJob(Job $job): PreparationState
+    public function getForResultsJob(Job $job): ComponentPreparation
     {
         if ($this->resultsJobRepository->count(['jobId' => $job->id]) > 0) {
-            return PreparationState::SUCCEEDED;
+            return new ComponentPreparation(PreparationState::SUCCEEDED);
         }
 
         $remoteRequest = $this->remoteRequestRepository->findNewest($job, RemoteRequestType::RESULTS_CREATE);
         if (null === $remoteRequest) {
-            return PreparationState::PENDING;
+            return new ComponentPreparation(PreparationState::PENDING);
         }
 
         if (RequestState::FAILED === $remoteRequest->getState()) {
-            return PreparationState::FAILED;
+            return new ComponentPreparation(PreparationState::FAILED, $remoteRequest->getFailure());
         }
 
-        return PreparationState::PREPARING;
+        return new ComponentPreparation(PreparationState::PREPARING);
     }
 
-    public function getForSerializedSuite(Job $job): PreparationState
+    public function getForSerializedSuite(Job $job): ComponentPreparation
     {
         if ($this->serializedSuiteRepository->count(['jobId' => $job->id]) > 0) {
-            return PreparationState::SUCCEEDED;
+            return new ComponentPreparation(PreparationState::SUCCEEDED);
         }
 
         $remoteRequest = $this->remoteRequestRepository->findNewest($job, RemoteRequestType::SERIALIZED_SUITE_CREATE);
         if (null === $remoteRequest) {
-            return PreparationState::PENDING;
+            return new ComponentPreparation(PreparationState::PENDING);
         }
 
         if (RequestState::FAILED === $remoteRequest->getState()) {
-            return PreparationState::FAILED;
+            return new ComponentPreparation(PreparationState::FAILED, $remoteRequest->getFailure());
         }
 
-        return PreparationState::PREPARING;
+        return new ComponentPreparation(PreparationState::PREPARING);
     }
 
-    public function getForMachine(Job $job): PreparationState
+    public function getForMachine(Job $job): ComponentPreparation
     {
         if ($this->machineRepository->count(['jobId' => $job->id]) > 0) {
-            return PreparationState::SUCCEEDED;
+            return new ComponentPreparation(PreparationState::SUCCEEDED);
         }
 
         $remoteRequest = $this->remoteRequestRepository->findNewest($job, RemoteRequestType::MACHINE_CREATE);
         if (null === $remoteRequest) {
-            return PreparationState::PENDING;
+            return new ComponentPreparation(PreparationState::PENDING);
         }
 
         if (RequestState::FAILED === $remoteRequest->getState()) {
-            return PreparationState::FAILED;
+            return new ComponentPreparation(PreparationState::FAILED, $remoteRequest->getFailure());
         }
 
-        return PreparationState::PREPARING;
+        return new ComponentPreparation(PreparationState::PREPARING);
     }
 }
