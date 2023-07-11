df40c9b1aba6f15089ef9277c366a2b8d35b13ae
Jon Cram <webignition@gmail.com>
2023-07-11 10:42:07 +0100

    Provide serialized suite preparation state

--- src/Services/PreparationStateDeriver.php
+++ src/Services/PreparationStateDeriver.php
@@ -5,35 +5,55 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Entity\Job;
-use App\Enum\PreparationState as PreparationStateEnum;
+use App\Enum\PreparationState;
 use App\Enum\RemoteRequestType;
 use App\Enum\RequestState;
 use App\Repository\RemoteRequestRepository;
 use App\Repository\ResultsJobRepository;
+use App\Repository\SerializedSuiteRepository;
 
 class PreparationStateDeriver
 {
     public function __construct(
         private readonly ResultsJobRepository $resultsJobRepository,
+        private readonly SerializedSuiteRepository $serializedSuiteRepository,
         private readonly RemoteRequestRepository $remoteRequestRepository,
     ) {
     }
 
-    public function getForResultsJob(Job $job): PreparationStateEnum
+    public function getForResultsJob(Job $job): PreparationState
     {
         if ($this->resultsJobRepository->count(['jobId' => $job->id]) > 0) {
-            return PreparationStateEnum::SUCCEEDED;
+            return PreparationState::SUCCEEDED;
         }
 
         $remoteRequest = $this->remoteRequestRepository->findNewest($job, RemoteRequestType::RESULTS_CREATE);
         if (null === $remoteRequest) {
-            return PreparationStateEnum::PENDING;
+            return PreparationState::PENDING;
         }
 
         if (RequestState::FAILED === $remoteRequest->getState()) {
-            return PreparationStateEnum::FAILED;
+            return PreparationState::FAILED;
         }
 
-        return PreparationStateEnum::PREPARING;
+        return PreparationState::PREPARING;
+    }
+
+    public function getForSerializedSuite(Job $job): PreparationState
+    {
+        if ($this->serializedSuiteRepository->count(['jobId' => $job->id]) > 0) {
+            return PreparationState::SUCCEEDED;
+        }
+
+        $remoteRequest = $this->remoteRequestRepository->findNewest($job, RemoteRequestType::SERIALIZED_SUITE_CREATE);
+        if (null === $remoteRequest) {
+            return PreparationState::PENDING;
+        }
+
+        if (RequestState::FAILED === $remoteRequest->getState()) {
+            return PreparationState::FAILED;
+        }
+
+        return PreparationState::PREPARING;
     }
 }
