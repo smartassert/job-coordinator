fd43cf80533eb0e44eb2b7261efbb63d214e11d5
Jon Cram <webignition@gmail.com>
2023-07-12 11:21:05 +0100

    Provide states for all worker compontents (#709)

---
+++ tests/Functional/Repository/WorkerComponentStateRepositoryTest.php
@@ -0,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Repository;
+
+use App\Entity\Job;
+use App\Entity\WorkerComponentState;
+use App\Enum\WorkerComponentName;
+use App\Repository\JobRepository;
+use App\Repository\WorkerComponentStateRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class WorkerComponentStateRepositoryTest extends WebTestCase
+{
+    private WorkerComponentStateRepository $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $entityManager = self::getContainer()->get(EntityManagerInterface::class);
+        \assert($entityManager instanceof EntityManagerInterface);
+
+        $repository = self::getContainer()->get(WorkerComponentStateRepository::class);
+        \assert($repository instanceof WorkerComponentStateRepository);
+        foreach ($repository->findAll() as $entity) {
+            $entityManager->remove($entity);
+            $entityManager->flush();
+        }
+        $this->repository = $repository;
+    }
+
+    /**
+     * @dataProvider getAllForJobDataProvider
+     *
+     * @param callable(Job, WorkerComponentStateRepository): void $statesCreator
+     * @param callable(Job): WorkerComponentState[]               $expectedStatesCreator
+     */
+    public function testGetAllForJob(callable $statesCreator, callable $expectedStatesCreator): void
+    {
+        $job = new Job(md5((string) rand()), md5((string) rand()), md5((string) rand()), rand(1, 1000));
+
+        $jobRepository = self::getContainer()->get(JobRepository::class);
+        \assert($jobRepository instanceof JobRepository);
+        $jobRepository->add($job);
+
+        $statesCreator($job, $this->repository);
+
+        self::assertEquals(
+            $this->repository->getAllForJob($job),
+            $expectedStatesCreator($job)
+        );
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getAllForJobDataProvider(): array
+    {
+        return [
+            'no states' => [
+                'statesCreator' => function () {
+                },
+                'expectedStatesCreator' => function () {
+                    return [];
+                },
+            ],
+            'no matching states' => [
+                'statesCreator' => function (Job $job, WorkerComponentStateRepository $repository) {
+                    $repository->save(
+                        (new WorkerComponentState(md5((string) rand()), WorkerComponentName::APPLICATION))
+                            ->setState('compiling')
+                            ->setIsEndState(false)
+                    );
+
+                    $repository->save(
+                        (new WorkerComponentState(md5((string) rand()), WorkerComponentName::COMPILATION))
+                            ->setState('running')
+                            ->setIsEndState(false)
+                    );
+
+                    $repository->save(
+                        (new WorkerComponentState(md5((string) rand()), WorkerComponentName::EXECUTION))
+                            ->setState('pending')
+                            ->setIsEndState(false)
+                    );
+
+                    $repository->save(
+                        (new WorkerComponentState(md5((string) rand()), WorkerComponentName::EVENT_DELIVERY))
+                            ->setState('running')
+                            ->setIsEndState(false)
+                    );
+                },
+                'expectedStatesCreator' => function () {
+                    return [];
+                },
+            ],
+            'single matching state' => [
+                'statesCreator' => function (Job $job, WorkerComponentStateRepository $repository) {
+                    $repository->save(
+                        (new WorkerComponentState($job->id, WorkerComponentName::APPLICATION))
+                            ->setState('executing')
+                            ->setIsEndState(false)
+                    );
+                },
+                'expectedStatesCreator' => function (Job $job) {
+                    return [
+                        (new WorkerComponentState($job->id, WorkerComponentName::APPLICATION))
+                            ->setState('executing')
+                            ->setIsEndState(false),
+                    ];
+                },
+            ],
+            'multiple matching state' => [
+                'statesCreator' => function (Job $job, WorkerComponentStateRepository $repository) {
+                    $repository->save(
+                        (new WorkerComponentState($job->id, WorkerComponentName::APPLICATION))
+                            ->setState('executing')
+                            ->setIsEndState(false)
+                    );
+
+                    $repository->save(
+                        (new WorkerComponentState($job->id, WorkerComponentName::COMPILATION))
+                            ->setState('complete')
+                            ->setIsEndState(true)
+                    );
+
+                    $repository->save(
+                        (new WorkerComponentState(md5((string) rand()), WorkerComponentName::EXECUTION))
+                            ->setState('pending')
+                            ->setIsEndState(false)
+                    );
+
+                    $repository->save(
+                        (new WorkerComponentState(md5((string) rand()), WorkerComponentName::EVENT_DELIVERY))
+                            ->setState('running')
+                            ->setIsEndState(false)
+                    );
+                },
+                'expectedStatesCreator' => function (Job $job) {
+                    return [
+                        (new WorkerComponentState($job->id, WorkerComponentName::APPLICATION))
+                            ->setState('executing')
+                            ->setIsEndState(false),
+                        (new WorkerComponentState($job->id, WorkerComponentName::COMPILATION))
+                            ->setState('complete')
+                            ->setIsEndState(true),
+                    ];
+                },
+            ],
+        ];
+    }
+}
