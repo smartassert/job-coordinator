84c5d06ba616950d5944da50f1eec6f6eb6ec5a8
Jon Cram <webignition@gmail.com>
2023-07-14 14:27:09 +0100

    Get component preparation state from component-specific handlers

--- src/Services/ComponentPreparationFactory.php
+++ src/Services/ComponentPreparationFactory.php
@@ -6,105 +6,49 @@ namespace App\Services;
 
 use App\Entity\Job;
 use App\Enum\JobComponentName;
-use App\Enum\PreparationState;
 use App\Enum\RemoteRequestType;
-use App\Enum\RequestState;
 use App\Model\ComponentPreparation;
 use App\Model\JobComponent;
-use App\Repository\MachineRepository;
-use App\Repository\RemoteRequestRepository;
-use App\Repository\ResultsJobRepository;
-use App\Repository\SerializedSuiteRepository;
-use App\Repository\WorkerComponentStateRepository;
+use App\Services\JobComponentHandler\JobComponentHandlerInterface;
 
 class ComponentPreparationFactory
 {
+    /**
+     * @param JobComponentHandlerInterface[] $jobComponentHandlers
+     */
     public function __construct(
-        private readonly ResultsJobRepository $resultsJobRepository,
-        private readonly SerializedSuiteRepository $serializedSuiteRepository,
-        private readonly MachineRepository $machineRepository,
-        private readonly RemoteRequestRepository $remoteRequestRepository,
-        private readonly WorkerComponentStateRepository $workerComponentStateRepository,
+        private readonly iterable $jobComponentHandlers,
     ) {
     }
 
     /**
-     * @return array{
-     *   results_job: ComponentPreparation,
-     *   serialized_suite: ComponentPreparation,
-     *   machine: ComponentPreparation,
-     *   worker_job: ComponentPreparation
-     * }
+     * @return array<value-of<JobComponentName>, ComponentPreparation>
      */
     public function getAll(Job $job): array
     {
-        return [
-            JobComponentName::RESULTS_JOB->value => $this->getForResultsJob($job),
-            JobComponentName::SERIALIZED_SUITE->value => $this->getForSerializedSuite($job),
-            JobComponentName::MACHINE->value => $this->getForMachine($job),
-            JobComponentName::WORKER_JOB->value => $this->getForWorkerJob($job),
+        $jobComponents = [
+            new JobComponent(JobComponentName::RESULTS_JOB, RemoteRequestType::RESULTS_CREATE),
+            new JobComponent(JobComponentName::SERIALIZED_SUITE, RemoteRequestType::SERIALIZED_SUITE_CREATE),
+            new JobComponent(JobComponentName::MACHINE, RemoteRequestType::MACHINE_CREATE),
+            new JobComponent(JobComponentName::WORKER_JOB, RemoteRequestType::MACHINE_START_JOB),
         ];
-    }
 
-    private function getForResultsJob(Job $job): ComponentPreparation
-    {
-        $jobComponent = new JobComponent(JobComponentName::RESULTS_JOB, RemoteRequestType::RESULTS_CREATE);
+        $componentPreparations = [];
 
-        if ($this->resultsJobRepository->count(['jobId' => $job->id]) > 0) {
-            return new ComponentPreparation($jobComponent, PreparationState::SUCCEEDED);
+        foreach ($jobComponents as $jobComponent) {
+            $componentPreparation = null;
+
+            foreach ($this->jobComponentHandlers as $jobComponentHandler) {
+                if (null === $componentPreparation) {
+                    $componentPreparation = $jobComponentHandler->getComponentPreparation($jobComponent, $job);
+
+                    if ($componentPreparation instanceof ComponentPreparation) {
+                        $componentPreparations[$jobComponent->name->value] = $componentPreparation;
+                    }
+                }
+            }
         }
 
-        return $this->deriveFromRemoteRequests($job, $jobComponent);
-    }
-
-    private function getForSerializedSuite(Job $job): ComponentPreparation
-    {
-        $jobComponent = new JobComponent(
-            JobComponentName::SERIALIZED_SUITE,
-            RemoteRequestType::SERIALIZED_SUITE_CREATE
-        );
-
-        if ($this->serializedSuiteRepository->count(['jobId' => $job->id]) > 0) {
-            return new ComponentPreparation($jobComponent, PreparationState::SUCCEEDED);
-        }
-
-        return $this->deriveFromRemoteRequests($job, $jobComponent);
-    }
-
-    private function getForMachine(Job $job): ComponentPreparation
-    {
-        $jobComponent = new JobComponent(JobComponentName::MACHINE, RemoteRequestType::MACHINE_CREATE);
-
-        if ($this->machineRepository->count(['jobId' => $job->id]) > 0) {
-            return new ComponentPreparation($jobComponent, PreparationState::SUCCEEDED);
-        }
-
-        return $this->deriveFromRemoteRequests($job, $jobComponent);
-    }
-
-    private function getForWorkerJob(Job $job): ComponentPreparation
-    {
-        $jobComponent = new JobComponent(JobComponentName::WORKER_JOB, RemoteRequestType::MACHINE_START_JOB);
-
-        $componentStates = $this->workerComponentStateRepository->getAllForJob($job);
-        if ([] !== $componentStates) {
-            return new ComponentPreparation($jobComponent, PreparationState::SUCCEEDED);
-        }
-
-        return $this->deriveFromRemoteRequests($job, $jobComponent);
-    }
-
-    private function deriveFromRemoteRequests(Job $job, JobComponent $jobComponent): ComponentPreparation
-    {
-        $remoteRequest = $this->remoteRequestRepository->findNewest($job, $jobComponent->requestType);
-        if (null === $remoteRequest) {
-            return new ComponentPreparation($jobComponent, PreparationState::PENDING);
-        }
-
-        if (RequestState::FAILED === $remoteRequest->getState()) {
-            return new ComponentPreparation($jobComponent, PreparationState::FAILED, $remoteRequest->getFailure());
-        }
-
-        return new ComponentPreparation($jobComponent, PreparationState::PREPARING);
+        return $componentPreparations;
     }
 }
---
+++ src/Services/JobComponentHandler/ResultsJobHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\JobComponentHandler;
+
+use App\Entity\Job;
+use App\Enum\JobComponentName;
+use App\Enum\PreparationState;
+use App\Enum\RequestState;
+use App\Model\ComponentPreparation;
+use App\Model\JobComponent;
+use App\Repository\RemoteRequestRepository;
+use App\Repository\ResultsJobRepository;
+
+class ResultsJobHandler implements JobComponentHandlerInterface
+{
+    public function __construct(
+        private readonly ResultsJobRepository $entityRepository,
+        private readonly RemoteRequestRepository $remoteRequestRepository,
+    ) {
+    }
+
+    public function getComponentPreparation(JobComponent $jobComponent, Job $job): ?ComponentPreparation
+    {
+        if (JobComponentName::RESULTS_JOB !== $jobComponent->name) {
+            return null;
+        }
+
+        if ($this->entityRepository->count(['jobId' => $job->id]) > 0) {
+            return new ComponentPreparation($jobComponent, PreparationState::SUCCEEDED);
+        }
+
+        return $this->deriveFromRemoteRequests($job, $jobComponent);
+    }
+
+    private function deriveFromRemoteRequests(Job $job, JobComponent $jobComponent): ComponentPreparation
+    {
+        $remoteRequest = $this->remoteRequestRepository->findNewest($job, $jobComponent->requestType);
+        if (null === $remoteRequest) {
+            return new ComponentPreparation($jobComponent, PreparationState::PENDING);
+        }
+
+        if (RequestState::FAILED === $remoteRequest->getState()) {
+            return new ComponentPreparation($jobComponent, PreparationState::FAILED, $remoteRequest->getFailure());
+        }
+
+        return new ComponentPreparation($jobComponent, PreparationState::PREPARING);
+    }
+}
