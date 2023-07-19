871870b89c50d8e7fc8711a21418c889fcbba543
Jon Cram <webignition@gmail.com>
2023-07-19 14:30:54 +0100

    remove error response usage in JobController

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -5,14 +5,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Job;
-use App\Enum\ErrorResponseType;
 use App\Event\JobCreatedEvent;
-use App\Exception\EmptyUlidException;
 use App\Repository\JobRepository;
 use App\Request\CreateJobRequest;
-use App\Response\ErrorResponse;
 use App\Services\JobSerializer;
-use App\Services\UlidFactory;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use SmartAssert\UsersSecurityBundle\Security\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -27,15 +23,8 @@ class JobController
         User $user,
         JobRepository $repository,
         JobSerializer $jobSerializer,
-        UlidFactory $ulidFactory,
         EventDispatcherInterface $eventDispatcher,
     ): JsonResponse {
-        try {
-            $id = $ulidFactory->create();
-        } catch (EmptyUlidException) {
-            return new ErrorResponse(ErrorResponseType::SERVER_ERROR, 'Generated job id is an empty string.');
-        }
-
         $job = new Job($user->getUserIdentifier(), $request->suiteId, $request->maximumDurationInSeconds);
         $repository->add($job);
 
