5e7092c46a460a73fdc62b7c09b0dc624a7de9d2
Jon Cram <webignition@gmail.com>
2023-08-29 14:50:13 +0100

    Create service status endpoint (#760)

---
+++ tests/Integration/StatusTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration;
+
+use App\Tests\Application\AbstractStatusTest;
+
+class StatusTest extends AbstractStatusTest
+{
+    use GetClientAdapterTrait;
+
+    protected function getExpectedReadyValue(): bool
+    {
+        return false;
+    }
+
+    protected function getExpectedVersion(): string
+    {
+        return $_SERVER['EXPECTED_VERSION'] ?? 'docker_compose_version';
+    }
+}
