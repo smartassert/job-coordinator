13f953261c33b1cdcd7dfa88417b9c004fefb92f
Jon Cram <webignition@gmail.com>
2023-12-06 14:17:56 +0000

    Use Symfony 6.4 (#778)

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -13,7 +13,7 @@ use Psr\EventDispatcher\EventDispatcherInterface;
 use SmartAssert\UsersSecurityBundle\Security\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class JobController
 {
