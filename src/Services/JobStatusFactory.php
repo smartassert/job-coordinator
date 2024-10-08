f40c6c3fff4c1607e434b60927351c4c67639de4
Jon Cram <webignition@gmail.com>
2024-10-08 16:47:37 +0100

    Use JobStatus model (#849)

---
+++ src/Services/JobStatusFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Entity\Job;
+use App\Model\JobStatus;
+use App\Model\RemoteRequestCollection;
+use App\Repository\MachineRepository;
+use App\Repository\RemoteRequestRepository;
+use App\Repository\ResultsJobRepository;
+use App\Repository\SerializedSuiteRepository;
+
+readonly class JobStatusFactory
+{
+    public function __construct(
+        private PreparationStateFactory $preparationStateFactory,
+        private ResultsJobRepository $resultsJobRepository,
+        private SerializedSuiteRepository $serializedSuiteRepository,
+        private MachineRepository $machineRepository,
+        private WorkerStateFactory $workerStateFactory,
+        private RemoteRequestRepository $remoteRequestRepository,
+    ) {
+    }
+
+    public function create(Job $job): JobStatus
+    {
+        return new JobStatus(
+            $job,
+            $this->preparationStateFactory->create($job),
+            $this->resultsJobRepository->find($job->id),
+            $this->serializedSuiteRepository->find($job->id),
+            $this->machineRepository->find($job->id),
+            $this->workerStateFactory->createForJob($job),
+            new RemoteRequestCollection(
+                $this->remoteRequestRepository->findBy(['jobId' => $job->id], ['id' => 'ASC'])
+            ),
+        );
+    }
+}
