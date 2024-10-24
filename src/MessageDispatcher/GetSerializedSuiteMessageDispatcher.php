615e8782fc0abf244a93b55f982297253eb7793a
Jon Cram <webignition@gmail.com>
2024-10-24 19:39:26 +0100

    Apply to SerializedSuiteRetrievedEvent

--- src/MessageDispatcher/GetSerializedSuiteMessageDispatcher.php
+++ src/MessageDispatcher/GetSerializedSuiteMessageDispatcher.php
@@ -50,7 +50,7 @@ class GetSerializedSuiteMessageDispatcher implements EventSubscriberInterface
     public function dispatchForSerializedSuiteRetrievedEvent(SerializedSuiteRetrievedEvent $event): void
     {
         $this->messageDispatcher->dispatch(new GetSerializedSuiteMessage(
-            $event->authenticationToken,
+            $event->getAuthenticationToken(),
             $event->getJobId(),
             $event->serializedSuite->getId(),
         ));
