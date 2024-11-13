39cd3f28eba2696f3b4fc146e6bd25ef38387801
Jon Cram <webignition@gmail.com>
2024-11-13 12:10:40 +0000

    Create Job model

---
+++ src/Model/Job.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Symfony\Component\Uid\Ulid;
+
+/**
+ * @phpstan-type SerializedJob array{
+ *   id: ?non-empty-string,
+ *   suite_id: ?non-empty-string,
+ *   maximum_duration_in_seconds: positive-int,
+ *   created_at: positive-int
+ *  }
+ */
+readonly class Job implements \JsonSerializable
+{
+    /**
+     * @param non-empty-string $id
+     * @param non-empty-string $userId
+     * @param non-empty-string $suiteId
+     * @param positive-int     $maximumDurationInSeconds
+     */
+    public function __construct(
+        private string $id,
+        private string $userId,
+        private string $suiteId,
+        private int $maximumDurationInSeconds
+    ) {
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getSuiteId(): string
+    {
+        return $this->suiteId;
+    }
+
+    /**
+     * @return positive-int
+     */
+    public function getMaximumDurationInSeconds(): int
+    {
+        return max($this->maximumDurationInSeconds, 1);
+    }
+
+    /**
+     * @return SerializedJob
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'suite_id' => $this->suiteId,
+            'maximum_duration_in_seconds' => $this->maximumDurationInSeconds,
+            'created_at' => max($this->getCreatedAt(), 1),
+        ];
+    }
+
+    /**
+     * @return SerializedJob
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+
+    private function getCreatedAt(): int
+    {
+        $idAsUlid = new Ulid($this->id);
+
+        return (int) $idAsUlid->getDateTime()->format('U');
+    }
+}
