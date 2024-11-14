b90c1639385a1cd6c7ba97b94a8a17e1c6c9ef35
Jon Cram <webignition@gmail.com>
2024-11-14 14:52:09 +0000

    Halt machine termination if results job does not have end state (#1068)

--- tests/Functional/MessageHandler/TerminateMachineMessageHandlerTest.php
+++ tests/Functional/MessageHandler/TerminateMachineMessageHandlerTest.php
@@ -39,7 +39,25 @@ class TerminateMachineMessageHandlerTest extends AbstractMessageHandlerTestCase
         $handler($message);
     }
 
-    public function testInvokeResultsJobHasEndState(): void
+    public function testInvokeNoResultsJob(): void
+    {
+        $resultsJobRepository = self::getContainer()->get(ResultsJobRepository::class);
+        \assert($resultsJobRepository instanceof ResultsJobRepository);
+
+        $jobFactory = self::getContainer()->get(JobFactory::class);
+        \assert($jobFactory instanceof JobFactory);
+        $job = $jobFactory->createRandom();
+
+        $handler = $this->createHandler($resultsJobRepository, HttpMockedWorkerManagerClientFactory::create());
+
+        $message = new TerminateMachineMessage(self::$apiToken, $job->getId());
+
+        $handler($message);
+
+        $this->assertSame([], $this->eventRecorder->all(MachineTerminationRequestedEvent::class));
+    }
+
+    public function testInvokeResultsJobDoesNotHaveEndState(): void
     {
         $resultsJobRepository = self::getContainer()->get(ResultsJobRepository::class);
         \assert($resultsJobRepository instanceof ResultsJobRepository);
@@ -51,8 +69,7 @@ class TerminateMachineMessageHandlerTest extends AbstractMessageHandlerTestCase
         $resultsJobFactory = self::getContainer()->get(ResultsJobFactory::class);
         \assert($resultsJobFactory instanceof ResultsJobFactory);
         $resultsJob = $resultsJobFactory->create($job);
-        $resultsJob->setState('end');
-        $resultsJob->setEndState('end');
+        $resultsJob->setState('executing');
         $resultsJobRepository->save($resultsJob);
 
         $handler = $this->createHandler($resultsJobRepository, HttpMockedWorkerManagerClientFactory::create());
@@ -72,6 +89,10 @@ class TerminateMachineMessageHandlerTest extends AbstractMessageHandlerTestCase
         $jobFactory = self::getContainer()->get(JobFactory::class);
         \assert($jobFactory instanceof JobFactory);
         $job = $jobFactory->createRandom();
+
+        $resultsJobFactory = self::getContainer()->get(ResultsJobFactory::class);
+        \assert($resultsJobFactory instanceof ResultsJobFactory);
+        $resultsJobFactory->create(job: $job, endState: 'ended');
 
         $workerManagerException = new \Exception('Failed to terminate machine');
 
@@ -98,6 +119,10 @@ class TerminateMachineMessageHandlerTest extends AbstractMessageHandlerTestCase
         $jobFactory = self::getContainer()->get(JobFactory::class);
         \assert($jobFactory instanceof JobFactory);
         $job = $jobFactory->createRandom();
+
+        $resultsJobFactory = self::getContainer()->get(ResultsJobFactory::class);
+        \assert($resultsJobFactory instanceof ResultsJobFactory);
+        $resultsJobFactory->create(job: $job, endState: 'ended');
 
         $machine = MachineFactory::create(
             $job->getId(),
