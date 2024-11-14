b34a661c5212e078256caac9f13a12dd7b79c8ad
Jon Cram <webignition@gmail.com>
2024-11-14 10:00:35 +0000

    Update SerializedSuiteFactory

--- src/Services/SerializedSuiteFactory.php
+++ src/Services/SerializedSuiteFactory.php
@@ -4,17 +4,15 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Entity\Job;
 use App\Entity\SerializedSuite;
 use App\Event\SerializedSuiteCreatedEvent;
-use App\Repository\JobRepository;
 use App\Repository\SerializedSuiteRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class SerializedSuiteFactory implements EventSubscriberInterface
 {
     public function __construct(
-        private readonly JobRepository $jobRepository,
+        private readonly JobStore $jobStore,
         private readonly SerializedSuiteRepository $serializedSuiteRepository,
     ) {
     }
@@ -33,8 +31,8 @@ class SerializedSuiteFactory implements EventSubscriberInterface
 
     public function createOnSerializedSuiteCreatedEvent(SerializedSuiteCreatedEvent $event): void
     {
-        $job = $this->jobRepository->find($event->getJobId());
-        if (!$job instanceof Job) {
+        $job = $this->jobStore->retrieve($event->getJobId());
+        if (null === $job) {
             return;
         }
 
