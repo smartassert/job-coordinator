beb85ce98cd1203a956045b7c73159c83ccfcfa2
Jon Cram <webignition@gmail.com>
2024-11-14 15:39:51 +0000

    update GetMachineMessageHandler

--- src/MessageHandler/GetMachineMessageHandler.php
+++ src/MessageHandler/GetMachineMessageHandler.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace App\MessageHandler;
 
 use App\Event\MachineRetrievedEvent;
-use App\Exception\MessageHandlerJobNotFoundException;
 use App\Exception\RemoteJobActionException;
 use App\Message\GetMachineMessage;
-use App\Services\JobStore;
 use SmartAssert\WorkerManagerClient\Client as WorkerManagerClient;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -17,7 +15,6 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 final class GetMachineMessageHandler
 {
     public function __construct(
-        private readonly JobStore $jobStore,
         private readonly WorkerManagerClient $workerManagerClient,
         private readonly EventDispatcherInterface $eventDispatcher,
     ) {
@@ -25,19 +22,13 @@ final class GetMachineMessageHandler
 
     /**
      * @throws RemoteJobActionException
-     * @throws MessageHandlerJobNotFoundException
      */
     public function __invoke(GetMachineMessage $message): void
     {
-        $job = $this->jobStore->retrieve($message->getJobId());
-        if (null === $job) {
-            throw new MessageHandlerJobNotFoundException($message);
-        }
-
         $previousMachine = $message->machine;
 
         try {
-            $machine = $this->workerManagerClient->getMachine($message->authenticationToken, $job->getId());
+            $machine = $this->workerManagerClient->getMachine($message->authenticationToken, $message->getJobId());
 
             $this->eventDispatcher->dispatch(new MachineRetrievedEvent(
                 $message->authenticationToken,
