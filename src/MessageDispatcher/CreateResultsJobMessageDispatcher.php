2991c80b70448055a9b1c29e8552f70bfc6ee9b9
Jon Cram <webignition@gmail.com>
2024-11-20 16:03:46 +0000

    Add CreateResultsJobMessageDispatcher::reDispatch()

--- src/MessageDispatcher/CreateResultsJobMessageDispatcher.php
+++ src/MessageDispatcher/CreateResultsJobMessageDispatcher.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\MessageDispatcher;
 
 use App\Event\JobCreatedEvent;
+use App\Event\MessageNotYetHandleableEvent;
 use App\Message\CreateResultsJobMessage;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -19,6 +20,9 @@ readonly class CreateResultsJobMessageDispatcher extends AbstractMessageDispatch
             JobCreatedEvent::class => [
                 ['dispatchForJobCreatedEvent', 100],
             ],
+            MessageNotYetHandleableEvent::class => [
+                ['redispatch', 100],
+            ],
         ];
     }
 
@@ -31,5 +35,19 @@ readonly class CreateResultsJobMessageDispatcher extends AbstractMessageDispatch
         $this->messageDispatcher->dispatchWithNonDelayedStamp(
             new CreateResultsJobMessage($event->getAuthenticationToken(), $event->getJobId())
         );
+    }
+
+    public function redispatch(MessageNotYetHandleableEvent $event): void
+    {
+        $message = $event->message;
+
+        if (
+            !$message instanceof CreateResultsJobMessage
+            || MessageHandlingReadiness::NEVER === $this->readinessAssessor->isReady($message->getJobId())
+        ) {
+            return;
+        }
+
+        $this->messageDispatcher->dispatch($message);
     }
 }
--- tests/Functional/MessageDispatcher/CreateResultsJobMessageDispatcherTest.php
+++ tests/Functional/MessageDispatcher/CreateResultsJobMessageDispatcherTest.php
@@ -6,13 +6,15 @@ namespace App\Tests\Functional\MessageDispatcher;
 
 use App\Enum\MessageHandlingReadiness;
 use App\Event\JobCreatedEvent;
+use App\Event\MessageNotYetHandleableEvent;
 use App\Message\CreateResultsJobMessage;
 use App\MessageDispatcher\CreateResultsJobMessageDispatcher;
 use App\MessageDispatcher\JobRemoteRequestMessageDispatcher;
+use App\Messenger\NonDelayedStamp;
 use App\ReadinessAssessor\ReadinessAssessorInterface;
 use App\Tests\Services\Factory\JobFactory;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\Messenger\Stamp\DelayStamp;
 use Symfony\Component\Messenger\Transport\InMemory\InMemoryTransport;
 
 class CreateResultsJobMessageDispatcherTest extends WebTestCase
@@ -50,15 +52,7 @@ class CreateResultsJobMessageDispatcherTest extends WebTestCase
 
         $this->dispatcher->dispatchForJobCreatedEvent($event);
 
-        $envelopes = $this->messengerTransport->getSent();
-        self::assertCount(1, $envelopes);
-
-        $expectedMessage = new CreateResultsJobMessage($authenticationToken, $job->getId());
-
-        $dispatchedEnvelope = $envelopes[0];
-        self::assertEquals($expectedMessage, $dispatchedEnvelope->getMessage());
-
-        self::assertSame([], $dispatchedEnvelope->all(DelayStamp::class));
+        $this->assertNonDelayedMessageIsDispatched($authenticationToken, $job->getId());
     }
 
     public function testDispatchNotReady(): void
@@ -69,15 +63,109 @@ class CreateResultsJobMessageDispatcherTest extends WebTestCase
             ->andReturn(MessageHandlingReadiness::NEVER)
         ;
 
-        $messageDispatcher = self::getContainer()->get(JobRemoteRequestMessageDispatcher::class);
-        \assert($messageDispatcher instanceof JobRemoteRequestMessageDispatcher);
-
-        $dispatcher = new CreateResultsJobMessageDispatcher($messageDispatcher, $readinessAssessor);
+        $dispatcher = $this->createDispatcher($readinessAssessor);
 
         $event = new JobCreatedEvent('api token', 'job id', 'suite id', []);
 
         $dispatcher->dispatchForJobCreatedEvent($event);
 
+        $this->assertNoMessagesAreDispatched();
+    }
+
+    public function testRedispatchNeverReady(): void
+    {
+        $readinessAssessor = \Mockery::mock(ReadinessAssessorInterface::class);
+        $readinessAssessor
+            ->shouldReceive('isReady')
+            ->andReturn(MessageHandlingReadiness::NEVER)
+        ;
+
+        $dispatcher = $this->createDispatcher($readinessAssessor);
+
+        $message = new CreateResultsJobMessage('api token', 'job id');
+        $event = new MessageNotYetHandleableEvent($message);
+
+        $dispatcher->redispatch($event);
+
+        $this->assertNoMessagesAreDispatched();
+    }
+
+    #[DataProvider('redispatchSuccessDataProvider')]
+    public function testRedispatchSuccess(MessageHandlingReadiness $readiness): void
+    {
+        $jobFactory = self::getContainer()->get(JobFactory::class);
+        \assert($jobFactory instanceof JobFactory);
+        $job = $jobFactory->createRandom();
+
+        $readinessAssessor = \Mockery::mock(ReadinessAssessorInterface::class);
+        $readinessAssessor
+            ->shouldReceive('isReady')
+            ->andReturn($readiness)
+        ;
+
+        $dispatcher = $this->createDispatcher($readinessAssessor);
+
+        $message = new CreateResultsJobMessage('api token', $job->getId());
+        $event = new MessageNotYetHandleableEvent($message);
+
+        $dispatcher->redispatch($event);
+
+        $this->assertDelayedMessageIsDispatched($message->authenticationToken, $job->getId());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public static function redispatchSuccessDataProvider(): array
+    {
+        return [
+            'ready now' => [
+                'readiness' => MessageHandlingReadiness::NOW,
+            ],
+            'ready eventually' => [
+                'readiness' => MessageHandlingReadiness::EVENTUALLY,
+            ],
+        ];
+    }
+
+    private function createDispatcher(ReadinessAssessorInterface $readinessAssessor): CreateResultsJobMessageDispatcher
+    {
+        $messageDispatcher = self::getContainer()->get(JobRemoteRequestMessageDispatcher::class);
+        \assert($messageDispatcher instanceof JobRemoteRequestMessageDispatcher);
+
+        return new CreateResultsJobMessageDispatcher($messageDispatcher, $readinessAssessor);
+    }
+
+    private function assertNoMessagesAreDispatched(): void
+    {
         self::assertSame([], $this->messengerTransport->getSent());
+    }
+
+    /**
+     * @param non-empty-string $authenticationToken
+     * @param non-empty-string $jobId
+     */
+    private function assertNonDelayedMessageIsDispatched(string $authenticationToken, string $jobId): void
+    {
+        $envelopes = $this->messengerTransport->getSent();
+        self::assertCount(1, $envelopes);
+
+        $envelope = $envelopes[0];
+        self::assertEquals(new CreateResultsJobMessage($authenticationToken, $jobId), $envelope->getMessage());
+        self::assertEquals([new NonDelayedStamp()], $envelope->all(NonDelayedStamp::class));
+    }
+
+    /**
+     * @param non-empty-string $authenticationToken
+     * @param non-empty-string $jobId
+     */
+    private function assertDelayedMessageIsDispatched(string $authenticationToken, string $jobId): void
+    {
+        $envelopes = $this->messengerTransport->getSent();
+        self::assertCount(1, $envelopes);
+
+        $envelope = $envelopes[0];
+        self::assertEquals(new CreateResultsJobMessage($authenticationToken, $jobId), $envelope->getMessage());
+        self::assertEquals([], $envelope->all(NonDelayedStamp::class));
     }
 }
