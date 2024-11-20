53ebc38b309903c303a5499c1d5d79bbfc900973
Jon Cram <webignition@gmail.com>
2024-11-20 10:50:40 +0000

    use readiness assessor for serialized suite retrieval

--- src/MessageDispatcher/GetSerializedSuiteMessageDispatcher.php
+++ src/MessageDispatcher/GetSerializedSuiteMessageDispatcher.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace App\MessageDispatcher;
 
+use App\Enum\MessageHandlingReadiness;
 use App\Event\SerializedSuiteCreatedEvent;
 use App\Event\SerializedSuiteRetrievedEvent;
 use App\Message\GetSerializedSuiteMessage;
+use App\ReadinessAssessor\ReadinessAssessorInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 readonly class GetSerializedSuiteMessageDispatcher implements EventSubscriberInterface
 {
     public function __construct(
         private JobRemoteRequestMessageDispatcher $messageDispatcher,
+        private ReadinessAssessorInterface $readinessAssessor,
     ) {
     }
 
@@ -34,6 +37,10 @@ readonly class GetSerializedSuiteMessageDispatcher implements EventSubscriberInt
     public function dispatchForSerializedSuiteEvent(
         SerializedSuiteCreatedEvent|SerializedSuiteRetrievedEvent $event
     ): void {
+        if (MessageHandlingReadiness::NOW !== $this->readinessAssessor->isReady($event->getJobId())) {
+            return;
+        }
+
         $this->messageDispatcher->dispatchWithNonDelayedStamp(new GetSerializedSuiteMessage(
             $event->getAuthenticationToken(),
             $event->getJobId(),
---
+++ src/ReadinessAssessor/GetSerializedSuiteReadinessAssessor.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ReadinessAssessor;
+
+use App\Enum\MessageHandlingReadiness;
+use App\Repository\SerializedSuiteRepository;
+
+readonly class GetSerializedSuiteReadinessAssessor implements ReadinessAssessorInterface
+{
+    public function __construct(
+        private SerializedSuiteRepository $serializedSuiteRepository,
+    ) {
+    }
+
+    public function isReady(string $jobId): MessageHandlingReadiness
+    {
+        $serializedSuite = $this->serializedSuiteRepository->find($jobId);
+        if (null === $serializedSuite || $serializedSuite->hasEndState()) {
+            return MessageHandlingReadiness::NEVER;
+        }
+
+        return MessageHandlingReadiness::NOW;
+    }
+}
