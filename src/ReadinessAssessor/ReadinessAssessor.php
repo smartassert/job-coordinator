d37532c8f6ac105886e22bd8f095d9f5505c15a4
Jon Cram <webignition@gmail.com>
2025-11-20 19:08:27 +0000

    Create delegrating assessor

---
+++ src/ReadinessAssessor/ReadinessAssessor.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ReadinessAssessor;
+
+use App\Enum\MessageHandlingReadiness;
+use App\Model\RemoteRequestType;
+
+readonly class ReadinessAssessor
+{
+    /**
+     * @var ReadinessAssessorInterface[]
+     */
+    private array $assessors;
+
+    /**
+     * @param iterable<mixed> $assessors
+     */
+    public function __construct(iterable $assessors)
+    {
+        $filteredAssessors = [];
+
+        foreach ($assessors as $assessor) {
+            if ($assessor instanceof ReadinessAssessorInterface) {
+                $filteredAssessors[] = $assessor;
+            }
+        }
+
+        $this->assessors = $filteredAssessors;
+    }
+
+    public function isReady(RemoteRequestType $type, string $jobId): MessageHandlingReadiness
+    {
+        foreach ($this->assessors as $assessor) {
+            if ($assessor->handles($type)) {
+                return $assessor->isReady($jobId);
+            }
+        }
+
+        return MessageHandlingReadiness::NEVER;
+    }
+}
