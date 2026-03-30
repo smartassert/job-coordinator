97908002dbc13286423a5f784731430a567653bf
Jon Cram <webignition@gmail.com>
2026-03-30 11:10:59 +0100

    Re-namespace job component models (#1267)

--- src/Model/JobComponents.php
+++ src/Model/JobComponents.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Model;
 
 use App\Enum\JobComponentName;
+use App\Model\JobComponent\NamedJobComponentInterface;
 
 readonly class JobComponents implements \JsonSerializable
 {
