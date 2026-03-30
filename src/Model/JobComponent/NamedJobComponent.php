c1cdd1f99915f58a94ae347178e5868a7ff32c15
Jon Cram <webignition@gmail.com>
2026-03-30 11:10:59 +0100

    Re-namespace job component models (#1267)

--- src/Model/JobComponent/NamedJobComponent.php
+++ src/Model/JobComponent/NamedJobComponent.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-namespace App\Model;
+namespace App\Model\JobComponent;
 
 use App\Enum\JobComponentName;
+use App\Model\SerializeToArrayInterface;
 
 readonly class NamedJobComponent implements NamedJobComponentInterface
 {
