0baf8bb4ef4832e073f2ef54c4bb0aa68ea9e6dd
Jon Cram <webignition@gmail.com>
2026-03-30 11:10:59 +0100

    Re-namespace job component models (#1267)

--- src/Services/JobStatusFactory.php
+++ src/Services/JobStatusFactory.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Enum\JobComponentName;
+use App\Model\JobComponent\NamedJobComponent;
+use App\Model\JobComponent\WorkerJobJobComponent;
 use App\Model\JobComponents;
 use App\Model\JobInterface;
 use App\Model\JobStatus;
-use App\Model\NamedJobComponent;
 use App\Model\RemoteRequestCollection;
 use App\Repository\MachineRepository;
 use App\Repository\RemoteRequestRepository;
