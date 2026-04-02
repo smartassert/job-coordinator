3b343b1af4f2c7d1132fd2e4e49534597ae8f6fc
Jon Cram <webignition@gmail.com>
2026-04-02 17:22:22 +0100

    Remove preparation.failures

--- src/Model/PreparationState.php
+++ src/Model/PreparationState.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Model;
 
-use App\Entity\RemoteRequestFailure;
 use App\Enum\JobComponentName;
 use App\Enum\PreparationState as PreparationStateEnum;
 use App\Enum\RequestState;
@@ -13,20 +12,17 @@ use App\Enum\RequestState;
  * @phpstan-type SerializedPreparationState array{
  *   state: PreparationStateEnum,
  *   request_states: array<RequestState>,
- *   failures: array<value-of<JobComponentName>, RemoteRequestFailure>,
  *   meta_state: MetaState
  * }
  */
 readonly class PreparationState implements \JsonSerializable
 {
     /**
-     * @param array<value-of<JobComponentName>, RequestState>         $requestStates
-     * @param array<value-of<JobComponentName>, RemoteRequestFailure> $componentFailures
+     * @param array<value-of<JobComponentName>, RequestState> $requestStates
      */
     public function __construct(
         private PreparationStateEnum $state,
         private array $requestStates,
-        private array $componentFailures,
     ) {}
 
     /**
@@ -37,7 +33,6 @@ readonly class PreparationState implements \JsonSerializable
         return [
             'state' => $this->state,
             'request_states' => $this->requestStates,
-            'failures' => $this->componentFailures,
             'meta_state' => $this->getMetaState(),
         ];
     }
