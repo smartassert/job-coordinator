8016f4b21c8c70257c60139e7b160f606e8100f0
Jon Cram <webignition@gmail.com>
2026-04-02 16:26:22 +0100

    Add preparation, requests keys to serialized-suite component

---
+++ src/Services/SerializedSuiteComponentFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Enum\JobComponentName;
+use App\Model\JobComponent\Preparation;
+use App\Model\JobComponent\SerializedSuite;
+use App\Model\JobInterface;
+use App\Model\RemoteRequestCollection;
+use App\Repository\RemoteRequestRepository;
+use App\Repository\SerializedSuiteRepository;
+use App\Services\JobComponentHandler\MachineHandler;
+
+readonly class SerializedSuiteComponentFactory
+{
+    public function __construct(
+        private SerializedSuiteRepository $serializedSuiteRepository,
+        private RemoteRequestRepository $remoteRequestRepository,
+        private MachineHandler $handler,
+    ) {}
+
+    public function createForJob(JobInterface $job): SerializedSuite
+    {
+        $componentPreparation = $this->handler->getComponentPreparation($job->getId());
+        $requestState = $this->handler->getRequestState($job->getId());
+
+        return new SerializedSuite(
+            $this->serializedSuiteRepository->find($job->getId()),
+            new RemoteRequestCollection(
+                $this->remoteRequestRepository->findAllForJobAndComponent(
+                    $job->getId(),
+                    JobComponentName::SERIALIZED_SUITE
+                )
+            ),
+            new Preparation($componentPreparation, $requestState),
+        );
+    }
+}
