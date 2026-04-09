28c973b5e91c5bf3a0dc105ac9d884d9f79f35d2
Jon Cram <webignition@gmail.com>
2026-04-09 12:15:55 +0100

    Remove preparation.request_states (#1332)

--- src/Services/PreparationStateFactory.php
+++ src/Services/PreparationStateFactory.php
@@ -13,12 +13,11 @@ class PreparationStateFactory
     public function __construct(
         private readonly ComponentPreparationFactory $componentPreparationFactory,
         private readonly PreparationStateReducer $preparationStateReducer,
-        private readonly RequestStatesFactory $requestStatesFactory,
     ) {}
 
     public function create(JobInterface $job): PreparationState
     {
-        return new PreparationState($this->createState($job->getId()), $this->requestStatesFactory->create($job));
+        return new PreparationState($this->createState($job->getId()));
     }
 
     public function createState(string $jobId): PreparationStateEnum
