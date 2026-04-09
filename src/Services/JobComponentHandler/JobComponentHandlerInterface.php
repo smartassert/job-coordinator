e543d998f86779450faa03f22fbf847ab4a7722a
Jon Cram <webignition@gmail.com>
2026-04-09 16:41:37 +0100

    Remove JobComponentHandlerInterface::getRequestState() from interface (#1337)

--- src/Services/JobComponentHandler/JobComponentHandlerInterface.php
+++ src/Services/JobComponentHandler/JobComponentHandlerInterface.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Services\JobComponentHandler;
 
 use App\Enum\JobComponentName;
-use App\Enum\RequestState;
 use App\Model\ComponentPreparation;
 
 interface JobComponentHandlerInterface
@@ -13,6 +12,4 @@ interface JobComponentHandlerInterface
     public function handles(JobComponentName $componentName): bool;
 
     public function getComponentPreparation(string $jobId): ComponentPreparation;
-
-    public function getRequestState(string $jobId): RequestState;
 }
