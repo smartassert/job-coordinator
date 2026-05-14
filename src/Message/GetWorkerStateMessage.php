a45e5e71f1a16ebf6c5217ad24c699a4df5bca59
Jon Cram <webignition@gmail.com>
2026-05-14 15:27:07 +0100

    create GetWorkerStateMessage

---
+++ src/Message/GetWorkerStateMessage.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message;
+
+use App\Model\RemoteRequestType;
+
+class GetWorkerStateMessage extends AbstractRemoteRequestMessage
+{
+    /**
+     * @param non-empty-string $jobId
+     * @param non-empty-string $machineIpAddress
+     */
+    public function __construct(
+        string $jobId,
+        public readonly string $machineIpAddress
+    ) {
+        parent::__construct($jobId);
+    }
+
+    public function getRemoteRequestType(): RemoteRequestType
+    {
+        return RemoteRequestType::createForWorkerStateRetrieval();
+    }
+}
