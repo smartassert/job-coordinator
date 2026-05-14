6dda34112eebcf867e562b763dde4e1ebc3f5b3e
Jon Cram <webignition@gmail.com>
2026-05-14 16:19:57 +0100

    create MachineIsReadyEvent event

---
+++ src/Event/MachineIsReadyEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Event;
+
+use App\Event\AuthenticatingEventInterface as AuthenticatingEvent;
+use SmartAssert\WorkerManagerClient\Model\Machine;
+
+class MachineIsReadyEvent extends AbstractMachineEvent implements JobEventInterface, AuthenticatingEvent
+{
+    use GetJobIdTrait;
+    use GetAuthenticationTokenTrait;
+
+    /**
+     * @param non-empty-string $authenticationToken
+     * @param non-empty-string $jobId
+     * @param non-empty-string $ipAddress
+     */
+    public function __construct(
+        private readonly string $authenticationToken,
+        private readonly string $jobId,
+        public readonly string $ipAddress,
+        Machine $machine,
+    ) {
+        parent::__construct($machine);
+    }
+}
