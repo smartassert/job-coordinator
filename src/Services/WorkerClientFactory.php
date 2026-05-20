5082635e3b9dd14b7370c05bcfeb3a3248968242
Jon Cram <webignition@gmail.com>
2026-05-20 11:05:59 +0100

    Use worker-client 10.0 (#1400)

--- src/Services/WorkerClientFactory.php
+++ src/Services/WorkerClientFactory.php
@@ -6,8 +6,10 @@ namespace App\Services;
 
 use SmartAssert\ServiceClient\Client as ServiceClient;
 use SmartAssert\WorkerClient\Client;
-use SmartAssert\WorkerClient\EventFactory;
-use SmartAssert\WorkerClient\JobFactory;
+use SmartAssert\WorkerClient\Factory\ApplicationStateFactory;
+use SmartAssert\WorkerClient\Factory\EventFactory;
+use SmartAssert\WorkerClient\Factory\JobCreationExceptionFactory;
+use SmartAssert\WorkerClient\Factory\JobFactory;
 
 class WorkerClientFactory
 {
@@ -15,10 +17,19 @@ class WorkerClientFactory
         private readonly ServiceClient $serviceClient,
         private readonly EventFactory $eventFactory,
         private readonly JobFactory $jobFactory,
+        private readonly ApplicationStateFactory $applicationStateFactory,
+        private readonly JobCreationExceptionFactory $jobCreationExceptionFactory,
     ) {}
 
     public function create(string $ipAddress): Client
     {
-        return new Client('https://' . $ipAddress, $this->serviceClient, $this->eventFactory, $this->jobFactory);
+        return new Client(
+            'https://' . $ipAddress,
+            $this->serviceClient,
+            $this->eventFactory,
+            $this->jobFactory,
+            $this->applicationStateFactory,
+            $this->jobCreationExceptionFactory,
+        );
     }
 }
