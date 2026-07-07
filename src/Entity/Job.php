cfa467853e50ee4179f9621fca5922fcdaadeb42
Jon Cram <webignition@gmail.com>
2026-07-07 17:02:36 +0100

    implement JobInterface on job entity

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Model\JobInterface;
 use App\Repository\JobRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Ulid;
 
+/**
+ * @phpstan-import-type SerializedJob from JobInterface
+ */
 #[ORM\Entity(repositoryClass: JobRepository::class)]
 #[ORM\Index(name: 'user_idx', columns: ['user_id'])]
 #[ORM\Index(name: 'user_suite_idx', columns: ['user_id', 'suite_id'])]
-readonly class Job
+readonly class Job implements JobInterface
 {
     #[ORM\Id]
     #[ORM\Column(length: 32, unique: true, nullable: false)]
@@ -37,5 +42,65 @@ readonly class Job
         $this->userId = $userId;
         $this->suiteId = $suiteId;
         $this->maximumDurationInSeconds = $maximumDurationInSeconds;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getSuiteId(): string
+    {
+        return $this->suiteId;
+    }
+
+    /**
+     * @return positive-int
+     */
+    public function getMaximumDurationInSeconds(): int
+    {
+        return max($this->maximumDurationInSeconds, 1);
+    }
+
+    /**
+     * @return SerializedJob
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'suite_id' => $this->suiteId,
+            'maximum_duration_in_seconds' => $this->maximumDurationInSeconds,
+            'created_at' => max($this->getCreatedAt(), 1),
+        ];
+    }
+
+    /**
+     * @return SerializedJob
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+
+    private function getCreatedAt(): int
+    {
+        $idAsUlid = new Ulid($this->id);
+
+        return (int) $idAsUlid->getDateTime()->format('U');
     }
 }
