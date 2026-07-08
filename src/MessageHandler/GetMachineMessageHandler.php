9ba7c60b3c01dc716253878632af0b244d69d8cc
Jon Cram <webignition@gmail.com>
2026-07-08 10:42:49 +0100

    Update GetMachineMessageHandler

--- src/MessageHandler/GetMachineMessageHandler.php
+++ src/MessageHandler/GetMachineMessageHandler.php
@@ -9,25 +9,21 @@ use App\Event\MachineRetrievedEvent;
 use App\Exception\RemoteJobActionException;
 use App\Message\GetMachineMessage;
 use App\ReadinessAssessor\ReadinessAssessorInterface;
-use Psr\Log\LoggerInterface;
+use App\Services\MessageStateMutator;
 use SmartAssert\WorkerManagerClient\Client as WorkerManagerClient;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\Exception\ExceptionInterface;
-use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsMessageHandler]
-final readonly class GetMachineMessageHandler extends AbstractMessageHandler
+final readonly class GetMachineMessageHandler
 {
     public function __construct(
         private ReadinessAssessorInterface $readinessAssessor,
+        private MessageStateMutator $messageStateMutator,
         private WorkerManagerClient $workerManagerClient,
-        EventDispatcherInterface $eventDispatcher,
-        MessageBusInterface $messageBus,
-        LoggerInterface $logger,
-    ) {
-        parent::__construct($eventDispatcher, $messageBus, $logger);
-    }
+        private EventDispatcherInterface $eventDispatcher,
+    ) {}
 
     /**
      * @throws RemoteJobActionException
@@ -36,7 +32,7 @@ final readonly class GetMachineMessageHandler extends AbstractMessageHandler
     public function __invoke(GetMachineMessage $message): void
     {
         $readiness = $this->readinessAssessor->isReady($message->getJobId());
-        $this->setMessageState($message, $readiness);
+        $this->messageStateMutator->set($message, $readiness);
 
         if (MessageHandlingReadiness::NOW !== $readiness) {
             return;
