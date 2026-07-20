a896fffb9ed4d1bc950f50bc7251f2050005dd2f
Jon Cram <webignition@gmail.com>
2026-07-20 11:40:02 +0100

    Simplfy serialized suite consumer test

--- tests/Functional/RemoteEvent/SourcesSerializedSuiteStateChangedWebhookConsumer/NoEventDispatchedTest.php
+++ tests/Functional/RemoteEvent/SourcesSerializedSuiteStateChangedWebhookConsumer/NoEventDispatchedTest.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace App\Tests\Functional\RemoteEvent\SourcesSerializedSuiteStateChangedWebhookConsumer;
 
 use App\Event\SerializedSuiteRetrievedEvent;
-use App\Repository\SerializedSuiteRepository;
 use App\Tests\Application\AbstractApplicationTest;
 use App\Tests\Functional\Application\GetClientAdapterTrait;
 use App\Tests\Services\EventSubscriber\EventRecorder;
-use App\Tests\Services\Factory\JobFactory;
 use App\Tests\Services\Factory\RemoteEventConfigurationFactory;
-use App\Tests\Services\Factory\SerializedSuiteFactory;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Symfony\Component\RemoteEvent\RemoteEvent;
 use Symfony\Component\Uid\Ulid;
 
@@ -19,12 +17,17 @@ class NoEventDispatchedTest extends AbstractApplicationTest
 {
     use GetClientAdapterTrait;
 
+    private EventRecorder $eventRecorder;
     private RemoteEventConfigurationFactory $remoteEventConfigurationFactory;
     private string $sourcesNotifySecret;
 
     protected function setUp(): void
     {
         parent::setUp();
+
+        $eventRecorder = self::getContainer()->get(EventRecorder::class);
+        \assert($eventRecorder instanceof EventRecorder);
+        $this->eventRecorder = $eventRecorder;
 
         $remoteEventConfigurationFactory = self::getContainer()->get(RemoteEventConfigurationFactory::class);
         \assert($remoteEventConfigurationFactory instanceof RemoteEventConfigurationFactory);
@@ -35,23 +38,9 @@ class NoEventDispatchedTest extends AbstractApplicationTest
         $this->sourcesNotifySecret = $sourcesNotifySecret;
     }
 
-    public function testRemoteEventNameDoesNotMatch(): void
+    #[DataProvider('noEventIsDispatchedDataProvider')]
+    public function testNoEventIsDispatched(RemoteEvent $event): void
     {
-        $jobFactory = self::getContainer()->get(JobFactory::class);
-        \assert($jobFactory instanceof JobFactory);
-        $job = $jobFactory->createRandom();
-
-        $serializedSuiteEntityFactory = self::getContainer()->get(SerializedSuiteFactory::class);
-        \assert($serializedSuiteEntityFactory instanceof SerializedSuiteFactory);
-
-        $serializedSuite = $serializedSuiteEntityFactory->createNewForJob($job, 'requested');
-
-        $event = new RemoteEvent(
-            name: 'non_matching_event_name',
-            id: (string) new Ulid(),
-            payload: [],
-        );
-
         $remoteEventConfiguration = $this->remoteEventConfigurationFactory->create($event, $this->sourcesNotifySecret);
 
         $response = self::$staticApplicationClient->makeSourcesSerializedSuiteStateChangedNotifyRequest(
@@ -60,54 +49,46 @@ class NoEventDispatchedTest extends AbstractApplicationTest
         );
 
         self::assertSame(202, $response->getStatusCode());
-
-        $serializedSuiteRepository = self::getContainer()->get(SerializedSuiteRepository::class);
-        \assert($serializedSuiteRepository instanceof SerializedSuiteRepository);
-
-        $retrievedSerializedSuite = $serializedSuiteRepository->findOneBy(['id' => $serializedSuite->id]);
-        self::assertSame($serializedSuite, $retrievedSerializedSuite);
+        self::assertEquals([], $this->eventRecorder->all(SerializedSuiteRetrievedEvent::class));
     }
 
-    public function testRemoteEventReferencesInvalidJobId(): void
+    /**
+     * @return array<mixed>
+     */
+    public static function noEventIsDispatchedDataProvider(): array
     {
-        $serializedSuiteEntityFactory = self::getContainer()->get(SerializedSuiteFactory::class);
-        \assert($serializedSuiteEntityFactory instanceof SerializedSuiteFactory);
-
-        $event = new RemoteEvent(
-            name: 'sources.serialized_suite.state_changed',
-            id: (string) new Ulid(),
-            payload: [
-                'id' => (string) new Ulid(),
-                'suite_id' => (string) new Ulid(),
-                'parameters' => [],
-                'state' => 'requested',
-                'meta_state' => [
-                    'pending' => true,
-                    'ended' => false,
-                    'succeeded' => false,
-                ],
-                'previous_states' => [],
-                'next_states' => [
-                    'preparing/running',
-                    'preparing/preparing',
-                    'prepared',
-                    'failed',
-                ],
+        return [
+            'event name incorrect' => [
+                'event' => new RemoteEvent(
+                    name: 'non_matching_event_name',
+                    id: (string) new Ulid(),
+                    payload: [],
+                ),
             ],
-        );
-
-        $remoteEventConfiguration = $this->remoteEventConfigurationFactory->create($event, $this->sourcesNotifySecret);
-
-        $response = self::$staticApplicationClient->makeSourcesSerializedSuiteStateChangedNotifyRequest(
-            $remoteEventConfiguration->headers,
-            $remoteEventConfiguration->body,
-        );
-
-        self::assertSame(202, $response->getStatusCode());
-
-        $eventRecorder = self::getContainer()->get(EventRecorder::class);
-        \assert($eventRecorder instanceof EventRecorder);
-
-        self::assertEquals([], $eventRecorder->all(SerializedSuiteRetrievedEvent::class));
+            'event references invalid job' => [
+                'event' => new RemoteEvent(
+                    name: 'sources.serialized_suite.state_changed',
+                    id: (string) new Ulid(),
+                    payload: [
+                        'id' => (string) new Ulid(),
+                        'suite_id' => (string) new Ulid(),
+                        'parameters' => [],
+                        'state' => 'requested',
+                        'meta_state' => [
+                            'pending' => true,
+                            'ended' => false,
+                            'succeeded' => false,
+                        ],
+                        'previous_states' => [],
+                        'next_states' => [
+                            'preparing/running',
+                            'preparing/preparing',
+                            'prepared',
+                            'failed',
+                        ],
+                    ],
+                ),
+            ],
+        ];
     }
 }
